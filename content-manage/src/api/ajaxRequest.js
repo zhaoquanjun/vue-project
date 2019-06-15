aa2308a00cc3c02139f5f7500dce9f81c9e12210
woxieao <867993946@qq.comhexo scd e:/git config --global user.email 867993946@qq.com>
2019-06-15 15:10:19 +0800

    配置路由

--- content-manage/src/api/ajaxRequest.js
+++ content-manage/src/api/ajaxRequest.js
@@ -27,7 +27,6 @@ axios.interceptors.request.use(
 
         //todo 测试阶段写死
         config.headers.appIdJust4Test = '823EB3BD-93F4-4655-B833-D604A6EF2022';
-        config.headers.userIdJust4Test = '9d0062ea-b906-4061-b0ce-0c236d25a916';
         return config;
     },
     error => {
@@ -37,7 +36,7 @@ axios.interceptors.request.use(
 // 响应拦截器
 axios.interceptors.response.use(
     response => {
-        console.log(response,'不一定成功吧')
+        console.log(response, '不一定成功吧')
         if (response.status === 200) {
             return Promise.resolve(response);
         } else {
@@ -46,7 +45,7 @@ axios.interceptors.response.use(
     },
     // 服务器状态码不是200的情况    
     error => {
-        console.log(error,"error");
+        console.log(error, "error");
         let status = error.response.status;
         if (error.response.status) {
             switch (error.response.status) {
@@ -54,7 +53,7 @@ axios.interceptors.response.use(
                 // 未登录则跳转登录页面，并携带当前页面的路径                
                 // 在登录成功后返回当前页面，这一步需要在登录页操作。                
                 case 401:
-                    router.push({path: '/401'})
+                    router.push({ path: '/401' })
                     break;
                 // 403 token过期                
                 // 登录过期对用户进行提示                
@@ -65,7 +64,7 @@ axios.interceptors.response.use(
                     break;
                 // 404请求不存在                
                 case 404:
-                    router.push({path: '/404'})
+                    router.push({ path: '/404' })
                     break;
                 // 其他错误，直接抛出错误提示                
                 default:
@@ -73,7 +72,7 @@ axios.interceptors.response.use(
                         message: status + error.response.data,
                         type: 'error',
                         duration: 5 * 1000
-                      })
+                    })
             }
             return Promise.reject(error.response);
         }
