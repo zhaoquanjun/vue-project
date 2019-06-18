0db1fcfe185b74d1b36ac30120008cfb79ed3b21
woxieao <867993946@qq.comhexo scd e:/git config --global user.email 867993946@qq.com>
2019-06-18 14:41:01 +0800

    产品分类前端完成

--- content-manage/src/components/ProductManage/CategoryTree.vue
+++ content-manage/src/components/ProductManage/CategoryTree.vue
@@ -1,120 +1,339 @@
 <template>
-  <div>
-    <el-tree
-      :data="categoryTreeData"
-      node-key="id"
-      default-expand-all
-      :expand-on-click-node="false"
-      @node-drag-start="handleDragStart"
-      @node-drag-enter="handleDragEnter"
-      @node-drag-leave="handleDragLeave"
-      @node-drag-over="handleDragOver"
-      @node-drag-end="handleDragEnd"
-      @node-drop="handleDrop"
-      accordion
-      ref="tree"
-      draggable
-      :allow-drop="allowDrop"
-      :allow-drag="allowDrag"
-    >
-      <span class="custom-tree-node" slot-scope="{ node, data }">
-        <input v-model="data.label">
-        <span>({{data.leafSum }})</span>
-        <span>
-          <el-button type="text" size="mini" @click="rename(data)">重命名</el-button>
-        </span>
-      </span>
-    </el-tree>
-  </div>
+    <div>
+        <el-tree :data="treeResult"
+                 node-key="id"
+                 default-expand-all
+                 :expand-on-click-node="false"
+                 @node-drag-end="handleDragEnd"
+                 @node-click="changeCategory"
+                 accordion
+                 ref="tree"
+                 draggable
+                 :allow-drop="allowDrop">
+            <div class="custom-tree-node"
+                 @mouseover="handlerOver(data)"
+                 @mouseleave="handlerMouseLeave"
+                 slot-scope="{ node, data }">
+                <div style="display: inline-block;"
+                     v-if="renameShowId === data.id || data.isNewAdd">
+                    <input class="category-name"
+                           v-model="data.label"
+                           v-filter-special-char
+                           @input="changeCategoryInput(data.label)">
+                    <span class="enter" @click.stop="hadnleTreeInput(data,data.isNewAdd)">
+                        <svg-icon icon-class="tree-yes"></svg-icon>
+                    </span>
+                    <span class="cancel" @click.stop="cancelhadnleTreeInput(data,node)">
+                        <svg-icon icon-class="tree-no"></svg-icon>
+                    </span>
+                </div>
+                <template v-else>
+                    <span>{{data.label}}</span>
+                    <span v-if="data.level<=1">({{data.leafSum }})</span>
+                </template>
+                <!-- 三个点 分类操作 -->
+                <span class="set-tree-type"
+                      @click.stop="handleShow(node,data)"
+                      v-show="data.id === treeNodeId">
+                    <svg-icon icon-class="tree-handler"></svg-icon>
+                </span>
+                <div class="tree-handle" v-show="node.data.id === curId">
+                    <button v-if="node.data.level <3"
+                            type="text"
+                            size="mini"
+                            @click.stop=" create(data,node)">
+                        添加子分类
+                    </button>
+                    <button v-if="node.data.level>0"
+                            type="text"
+                            size="mini"
+                            @click.stop="rename(data)">
+                        修改名称
+                    </button>
+
+                    <button v-if="node.data.level>0"
+                            type="text"
+                            size="mini"
+                            @click.stop="batchRemove( node,data)">
+                        删除分类
+                    </button>
+                </div>
+            </div>
+        </el-tree>
+    </div>
 </template>
 <script>
-let id = 1000;
-export default {
-  props: ["categoryTreeData", "searchOption"],
-  data() {
-    return {
-      flag:false
-    }
-  },
-  methods: {
-    handleDragStart(node, ev) {
-      console.log("drag start", node);
-    },
-    handleDragEnter(draggingNode, dropNode, ev) {
-      console.log("tree drag enter: ", dropNode.label);
-    },
-    handleDragLeave(draggingNode, dropNode, ev) {
-      console.log("tree drag leave: ", dropNode.label);
-    },
-    handleDragOver(draggingNode, dropNode, ev) {
-      console.log("tree drag over: ", dropNode.label);
-    },
-    handleDragEnd(draggingNode, dropNode, dropType, ev) {
-      console.log("tree drag end: ", dropNode && dropNode.label, dropType);
-    },
-    handleDrop(draggingNode, dropNode, dropType, ev) {
-      console.log("tree drop: ", dropNode.label, dropType);
-    },
-    allowDrop(draggingNode, dropNode, type) {
-      if (dropNode.data.label === "二级分类 3-1") {
-        return type !== "inner";
-      } else {
-        return true;
-      }
-    },
-    allowDrag(draggingNode) {
-      return draggingNode.data.label.indexOf("三级分类 3-2-2") === -1;
-    },
-    append(data) {
-      const newChild = { id: id++, label: "testtest", children: [] };
-      if (!data.children) {
-        this.$set(data, "children", []);
-      }
-      data.children.push(newChild);
-    },
+    let id = 1000;
+    export default {
+        props: ["treeResult", "productSearchOptions", "isrightPannel"],
+        data() {
+            return {
+                flag: false,
+                curId: null,
+                treeNodeId: null,
+                renameShowId: null,
+                isNewAdd: false,// false 允许创建子节点
+                curlabelName: "",
+                isRename: false,
+                newAddNode: "",
+                renameData: "",
+            };
+        },
+        methods: {
+            handlerOver(data) {
+                if (!isNaN(data.id)) this.treeNodeId = data.id;
+                if (this.isNewAdd) this.treeNodeId = null;
+            },
+            handlerMouseLeave() {
+                this.treeNodeId = this.curId = null;
+            },
+            //修改节点名称 OR 新增节点
+            hadnleTreeInput(data, isNewAdd) {
+                this.isNewAdd = false; //点击确定修改后 开启允许创建子节点
+                if (data.label == "") {
+                    this.$message({
+                        message: "分类名称不能为空",
+                        type: "warning"
+                    });
+                }
+                if (isNewAdd) {
+                    this.$emit("create", {
+                        DisplayName: data.label,
+                        ParentId: data.parentId
+                    });
+                    return;
+                }
+                if (data.label != "") {
+                    this.$emit("rename", data.id, data.label);
+                    this.renameShowId = this.curId = null;
+                }
+                this.treeNodeId = null;
+            },
+            //
+            cancelhadnleTreeInput(data, node) {
+                if (this.isRename) {
+                    if (data.label == "") {
+                        this.$message({
+                            message: "分类名称不能为空",
+                            type: "warning"
+                        });
+                        return
+                    }
+                    if (this.curlabelName != "") {
+                        data.label = this.curlabelName;
+                    }
 
-    remove(node, data) {
-      const parent = node.parent;
-      const children = parent.data.children || parent.data;
-      const index = children.findIndex(d => d.id === data.id);
-      children.splice(index, 1);
-    },
+                } else {
 
-    renderContent(h, { node, data, store }) {
-      return (
-        <span class="custom-tree-node">
-          <span>{node.label}</span>
-          <span>
-            <el-button
-              size="mini"
-              type="text"
-              on-click={() => this.append(data)}
-            >
-              Append
-            </el-button>
-            <el-button
-              size="mini"
-              type="text"
-              on-click={() => this.remove(node, data)}
-            >
-              Delete
-            </el-button>
-          </span>
-        </span>
-      );
-    }
-  },
-  rename(data) {
-    this.$emit("rename", data.id, data.label);
-  }
-};
+                    node.parent.childNodes.splice(0, 1)
+                    this.isRename = false;
+                    this.isNewAdd = false;
+                }
+                // 点击取消按钮 关闭input框
+                this.renameShowId = this.curId = null;
+            },
+            handleDragEnd(draggingNodeDom, targetNodeDom, dropType, ev) {
+                var draggingNode = draggingNodeDom.data;
+                var targetNode = targetNodeDom.data;
+                if (dropType === "none") {
+                    return;
+                }
+                switch (dropType) {
+                    case "inner": {
+                        draggingNode.parentId = targetNode.id;
+                        break;
+                    }
+                    case "before":
+                    case "after": {
+                        draggingNode.parentId = targetNode.parentId;
+                        break;
+                    }
+                    case "none":
+                    default: {
+                        return;
+                    }
+                }
+                var idOrderByArr = [];
+                for (
+                    var i = targetNodeDom.parent.childNodes.length - 1;
+                    i >= 0;
+                    i--
+                ) {
+                    var childNode = targetNodeDom.parent.childNodes[i];
+                    idOrderByArr.push(childNode.data.id);
+                }
+                this.modifyNode(
+                    draggingNode.id,
+                    draggingNode.parentId,
+                    idOrderByArr
+                );
+            },
+            allowDrop(draggingNode, targetNode, dropType) {
+                draggingNode = draggingNode.data;
+                targetNode = targetNode.data;
+                //判断是否大于三层
+                if (
+                    dropType === "inner" ||
+                    draggingNode.parentId !== targetNode.parentId
+                ) {
+                    return this.getLevel(draggingNode, 1) + targetNode.level <= 3;
+                }
+                return true;
+            },
+
+            create(data, node) {
+                if (!this.isNewAdd) {
+                    this.isNewAdd = true;
+                    const newChild = {
+                        parentId: data.id,
+                        label: "",
+                        isNewAdd: true
+                    };
+                    data.children.unshift(newChild);
+                }
+                this.curId = null;
+                this.newAddData = data;
+
+            },
+            getAllNodeIds(node, isChildNode) {
+                var idList = isChildNode ? [] : [node.id];
+                for (var i in node.children) {
+                    let child = node.children[i];
+                    idList.push(child.id);
+                    idList = idList.concat(this.getAllNodeIds(child, true));
+                }
+                return idList;
+            },
+            getLevel(node, level) {
+                var localLevel = level;
+                for (var i in node.children) {
+                    let child = node.children[i];
+                    var childLevel = this.getLevel(child, localLevel + 1);
+                    level = level < childLevel ? childLevel : level;
+                }
+                return level;
+            },
+            rename(data) {
+                this.renameShowId = data.id;
+                this.curId = null;
+                this.isRename = true;
+                this.renameData = data;
+                this.isNewAdd = true;
+            },
+            modifyNode(id, parentId, idOrderByArr) {
+                this.$emit("modifyNode", id, parentId, idOrderByArr);
+            },
+            // 描述：
+            batchRemove(node, data) {
+                console.log(data, 'remove-----')
+                this.$emit("batchRemove", this.getAllNodeIds(data));
+            },
+            changeCategory(data) {
+                if (this.isrightPannel) {
+                    console.log(data)
+                    this.$emit("chooseNode", data)
+                    return false
+                }
+                if (data.isNewAdd) return;
+                this.curlabelName = data.label;
+                this.productSearchOptions.proCategoryId = data.id;
+                this.$emit("getProList");
+                // 点击其他区域 把当前新增但未确定的节点删除掉
+                this.newAddData && this.newAddData.children.shift()
+                if (this.renameShowId !== data.id) this.isNewAdd = false;
+
+                //  // 点击其他区域 把当前重命名但未确定的，恢复重命名之前label
+                if (this.renameData && this.renameShowId !== data.id) {
+                    this.renameData.label = this.curlabelName;
+                    this.renameShowId = null;
+                }
+            },
+            handleShow(node, val) {
+                node.checked = true;
+                this.curId = node.data.id;
+            },
+            changeCategoryInput(label) {
+                console.log(label);
+            }
+        },
+        mounted() {
+            // document.addEventListener("click",()=>{
+            //    this.curId = null;
+            // })
+        }
+    };
 </script>
-<style scoped>
-.el-tree /deep/ .el-tree-node__content {
-  height: 44px;
-}
-.el-tree /deep/ .el-tree-node__label {
-  font-size: 12px;
-}
+<style>
+    .el-tree-node > .el-tree-node__children {
+        overflow: visible !important;
+    }
 </style>
 
+<style scoped>
+    .el-tree /deep/ .el-tree-node__content {
+        height: 44px;
+        position: relative !important;
+    }
+
+    .el-tree /deep/ .el-tree-node__label {
+        font-size: 12px;
+    }
+</style>
+<style lang="scss" scoped>
+    .custom-tree-node {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        display: block;
+        // width: 84px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        .enter
+
+    {
+        margin: 0 5px;
+    }
+
+    .category-name {
+        width: 86px;
+        height: 30px;
+        border: none;
+        border: 1px solid rgba(229, 229, 229, 1);
+    }
+
+    }
+
+    .tree-handle {
+        position: absolute;
+        right: -88px;
+        top: 0;
+        z-index: 1000;
+        background: #ffffff;
+        width: 92px;
+        background: rgba(255, 255, 255, 1);
+        box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.07);
+        padding: 5px 0;
+        button
+
+    {
+        width: 100%;
+        display: block;
+        padding: 8px 14px;
+        text-align: left;
+        &:hover
+
+    {
+        background: #eee;
+    }
+
+    }
+    }
+
+    .set-tree-type {
+        position: absolute;
+        right: 5px;
+        padding: 0 5px;
+        z-index: 10;
+    }
+</style>
--- content-manage/src/components/ProductManage/index.vue
+++ content-manage/src/components/ProductManage/index.vue
@@ -1,112 +1,176 @@
 <template>
-  <el-container>
-    <el-aside class="tree-aside">
-      <h4 class="product-type-title">
-        <svg-icon icon-class="img-type-title"></svg-icon>
-        <span>产品分类</span>
-      </h4>
-      <h5 class="title-item">全部分类{{totalSum}}</h5>
-      <category-tree
-        :category-tree-data="categoryTreeData"
-        :search-option="searchOption"
-        @getProductList="_getProductList"
-        @rename="renameCategory"
-      ></category-tree>
-    </el-aside>
-    <el-main>
-      <product-header
-        :search-option="searchOption"
-        @getProductList="_getProductList"
-        @showAddDialog="_showAddDialog"
-      />
-      <el-main>
-        <product-table
-          :product-list-data="productListData"
-          :search-option="searchOption"
-          :category-tree-data="categoryTreeData"
-          @getProductList="_getProductList"
-        ></product-table>
-      </el-main>
-    </el-main>
-     <el-dialog>
-      <add-product :tree-result="categoryTreeData"/>
-    </el-dialog>
-  </el-container>
+    <el-container id="product-manage">
+        <el-aside class="tree-aside">
+            <h4 class="product-type-title">
+                <svg-icon icon-class="img-type-title"></svg-icon>
+                <span>产品分类</span>
+            </h4>
+            <h5 class="title-item">全部分类</h5>
+            <category-tree :tree-result="treeResult"
+                           :product-search-options="productSearchOptions"
+                           @getProList="getProList"
+                           @create="newCategory"
+                           @batchRemove="batchRemoveCategory"
+                           @rename="renameCategory"
+                           @modifyNode="modifyNodeCategory"></category-tree>
+        </el-aside>
+        <el-main>
+            <product-header :product-search-options="productSearchOptions"
+                            @getProductList="getProList"
+                            @showAddDialog="showAddDialog" />
+            <el-main>
+                <product-table :product-list-data="productListData"
+                               :product-search-options="productSearchOptions"
+                               :category-tree-data="categoryTreeData"
+                               @getProductList="getProList"></product-table>
+            </el-main>
+        </el-main>
+        <el-dialog>
+            <add-product :tree-result="categoryTreeData" />
+        </el-dialog>
+    </el-container>
 </template>
 <script>
-import CategoryTree from "./CategoryTree";
-import ProductHeader from "./ProductHeader";
-import ProductTable from "./ProductTable";
-import AddProduct from "./AddProduct";
-import * as productManageApi from "@/api/request/productManageApi";
+    import CategoryTree from "./CategoryTree";
+    import ProductHeader from "./ProductHeader";
+    import ProductTable from "./ProductTable";
+    import AddProduct from "./AddProduct";
+    import * as productManageApi from "@/api/request/productManageApi";
+    import * as productCategoryManageApi from "@/api/request/productCategoryManageApi";
 
-export default {
-  components: {
-    CategoryTree,
-    ProductHeader,
-    ProductTable,
-    AddProduct
-  },
-  data() {
-    return {
-      productListData: {},
-      categoryTreeData: null,
-      searchOption: {
-        pageSize: 5,
-        pageIndex: 1,
-        sortOrder: "name_asc",
-        categoryId: null,
-        name: ""
-      },
-      totalSum: 0,
-      addDialogVisible: true
+    export default {
+        components: {
+            CategoryTree,
+            ProductHeader,
+            ProductTable,
+            AddProduct
+        },
+        data() {
+            return {
+                productListData: {},
+                categoryTreeData: null,
+                productSearchOptions: {
+                    pageSize: 10,
+                    pageIndex: 1,
+                    orderByType: 1,
+                    isDescending: true,
+                    proCategoryId: null,
+                    keyword: "",
+                    isDelete: false
+                },
+                totalSum: 0,
+                addDialogVisible: true,
+                treeResult: null
+            };
+        },
+        mounted() {
+            this.getProList();
+            this.getTree();
+        },
+        methods: {
+            async getProList() {
+                let { data } = await productManageApi.getProductList(this.productSearchOptions);
+                this.productListData = data;
+            },
+            async getTree() {
+                let { data } = await productCategoryManageApi.get();
+                this.treeResult = data.treeArray;
+                this.totalSum = data.totalSum;
+            },
+            async newCategory(entity) {
+                console.log(entity);
+                await productCategoryManageApi.create(entity);
+                this.getTree();
+            }, async batchRemoveCategory(idList) {
+                this.$confirm(
+                    "若该分类下存在数据，删除后数据将自动移动到“全部分类”中，是否确认删除该分类？",
+                    "提示",
+                    {
+                        confirmButtonText: "确定",
+                        cancelButtonText: "取消",
+                        type: "warning",
+                        callback: async action => {
+                            console.log(action);
+                            if (action === "confirm") {
+                                let {
+                                    status
+                                } = await productCategoryManageApi.batchRemove(idList);
+                                if (status === 200) {
+                                    this.getTree();
+                                    this.$message({
+                                        type: "success",
+                                        message: "删除成功!"
+                                    });
+                                }
+                            } else {
+                                this.$message({
+                                    type: "info",
+                                    message: "已取消删除"
+                                });
+                            }
+                        }
+                    }
+                );
+            },
+            async renameCategory(id, newName) {
+                await productCategoryManageApi.rename(id, newName);
+                this.getTree();
+            },
+            async modifyNodeCategory(id, parentId, idOrderByArr) {
+                await productCategoryManageApi.modifyNode(id, parentId, idOrderByArr);
+                this.getTree();
+            },
+            showAddDialog() { }
+        }
     };
-  },
-  mounted() {
-    this._getProductList({});
-    this._getCategoryTree();
-  },
-  methods: {
-    async _getProductList() {
-      let { data } = await productManageApi.getProductList(this.searchOption);
-      this.productListData = data;
-    },
-    async _getCategoryTree() {
-      let { data } = await productManageApi.getCategoryTree();
-      this.categoryTreeData = data.categoryTree;
-    },
-    async renameCategory(id, newName) {},
-    async _showAddDialog() {}
-  }
-};
 </script>
 <style scoped>
-.el-main {
-  /* padding: 0; */
-}
-.el-container /deep/ .tree-aside {
-  width: 164px !important;
-  height: 100vh;
-  background: #fff;
-  margin: 0 0 0 13px;
-}
+    #product-manage {
+        padding-bottom: 30px;
+    }
+
+        #product-manage .el-aside {
+            overflow: visible !important;
+        }
+
+        #product-manage .tree-aside {
+            width: 220px !important;
+            height: 100vh;
+            background: #fff;
+            margin: 0 0 0 13px;
+        }
+
+    .el-main {
+        /* padding: 0; */
+    }
+
+    .el-container /deep/ .tree-aside {
+        width: 164px !important;
+        height: 100vh;
+        background: #fff;
+        margin: 0 0 0 13px;
+    }
 </style>
 <style lang="scss" scoped>
-.product-type-title {
-  height: 60px;
-  line-height: 60px;
-  padding-left: 14px;
-  border-bottom: 1px solid #e8eaf3;
-  border-right: 1px solid #e8eaf3;
-  box-sizing: border-box;
-  span {
-    vertical-align: middle;
-    padding-left: 10px;
-  }
-}
-.title-item {
-  padding: 28px 0 12px 12px;
-}
+    .product-type-title {
+        height: 60px;
+        line-height: 60px;
+        padding-left: 14px;
+        border-bottom: 1px solid #e8eaf3;
+        border-right: 1px solid #e8eaf3;
+        box-sizing: border-box;
+        span
+
+    {
+        vertical-align: middle;
+        padding-left: 10px;
+    }
+
+    }
+
+    .title-item {
+        padding: 28px 0 12px 12px;
+    }
 </style>
 
 
