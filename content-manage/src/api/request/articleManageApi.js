18edcbf1311ca34a196fc58b479161fea4b2c5e5
litao <litao@clouddream.net>
2019-06-18 20:10:01 +0800

    文章分类前端页面事件

--- content-manage/src/api/request/articleManageApi.js
+++ content-manage/src/api/request/articleManageApi.js
@@ -10,6 +10,42 @@ import * as ajaxRequest from "../ajaxRequest";
  * 获取文章列表
  * @param {*} option 
  */
-export const getArticalList = (options) => {
+export const getArticleList = (options) => {
     return ajaxRequest.get(`/api/News/GetCategoryTree/${options.pageIndex}/${options.pageSize}`,options);
 }
+
+/**
+ * 获取文章分类
+ */
+export const getArticleCategory = () => {
+    return ajaxRequest.get(`/api/NewsCategory/GetTree`);
+}
+
+/**
+ * 重命名文章分类
+ * @param {*} id 
+ * @param {*} newName 
+ */
+export const reName = (id, newName) => {
+    console.log(id);
+    console.log(newName);
+    return ajaxRequest.put(`/api/NewsCategory/EditName`,{ CategoryId: id, CategoryName: newName });
+}
+
+
+/**
+ * 新增文章分类
+ * @param {*} entity 
+ */
+export const create = entity => {
+    return ajaxRequest.post(`/api/NewsCategory/Create`, entity );
+}
+
+/**
+ * 删除文章分类
+ * @param {*} idList 
+ */
+export const deleteNewsCategory = (idList) => {
+    return ajaxRequest._delete(`/api/NewsCategory/Delete`,  { data: idList } );
+}
+
--- content-manage/src/components/ArticleManage/MTree.vue
+++ content-manage/src/components/ArticleManage/MTree.vue
@@ -1,5 +1,6 @@
 <template>
-     <el-tree
+    <div>
+        <el-tree
             :data="treeResult"
             node-key="id"
             default-expand-all
@@ -23,9 +24,8 @@
                 >
                     <input
                         class="category-name"
-                        v-model="data.label"
+                        v-model="data.lable"
                         v-filter-special-char
-                        @input="changeCategoryInput(data.label)"
                     >
                     <span class="enter" @click.stop="hadnleTreeInput(data,data.isNewAdd)">
                         <svg-icon icon-class="tree-yes"></svg-icon>
@@ -35,8 +35,8 @@
                     </span>
                 </div>
                 <template v-else>
-                    <span>{{data.label}}</span>
-                    <span v-if="data.level<=1">({{data.leafSum }})</span>
+                    <span>{{data.lable}}</span>
+                    <span v-if="data.level<=1">({{data.childrenSum }})</span>
                 </template>
                 <!-- 三个点 分类操作 -->
                 <span
@@ -51,7 +51,7 @@
                         v-if="node.data.level <3"
                         type="text"
                         size="mini"
-                        @click.stop=" create(data,node)"
+                        @click.stop="create(data,node)"
                     >添加子分类</button>
                     <button
                         v-if="node.data.level>0"
@@ -68,65 +68,15 @@
                     >删除分类</button>
                 </div>
             </div>
-        </el-tree>
+        </el-tree>   
+    </div>         
 </template>
 <script>
 let id = 1000;
-export default {   
-   // props: ["treeResult", "airSearchOptions"],
+export default {  
+    props: ["treeResult", "articleSearchOptions"],
     data() {
-            const data = [
-            {
-                id: 1,
-                label: "一级分类 1",
-                children: [
-                    {
-                        id: 4,
-                        label: "二级分类 1-1",
-                        children: [
-                            {
-                                id: 9,
-                                label: "三级分类 1-1-1"
-                            },
-                            {
-                                id: 10,
-                                label: "三级分类 1-1-2"
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                id: 2,
-                label: "一级分类 2",
-                children: [
-                    {
-                        id: 5,
-                        label: "二级分类 2-1"
-                    },
-                    {
-                        id: 6,
-                        label: "二级分类 2-2"
-                    }
-                ]
-            },
-            {
-                id: 3,
-                label: "一级分类 3",
-                children: [
-                    {
-                        id: 7,
-                        label: "二级分类 3-1"
-                    },
-                    {
-                        id: 8,
-                        label: "二级分类 3-2"
-                    }
-                ]
-            }
-        ];
         return {
-            treeResult:data,
             flag: false,
             curId: null,
             treeNodeId: null,
@@ -136,18 +86,11 @@ export default {
             isRename:false,
             newAddNode:"",
             renameData:"",
-            airSearchOptions: {
-                pageSize: 10,
-                pageIndex: 1,
-                orderByType: 1,
-                isDescending: true,
-                airCategoryId: null,
-                keyword: "",
-                isDelete: false
-            }
         }
     },
-
+    mounted() {
+       
+    },
     methods: {
         handlerOver(data) {
             if(data.id) this.treeNodeId = data.id;
@@ -158,30 +101,32 @@ export default {
         },
        //修改节点名称 OR 新增节点
         hadnleTreeInput(data, isNewAdd) {
+            console.log(data);
+            console.log(isNewAdd);
             this.isNewAdd = false; //点击确定修改后 开启允许创建子节点
-            if (data.label == "") {
+            if (data.lable == "") {
                 this.$message({
                     message: "分类名称不能为空",
                     type: "warning"
                 });
-            }
-            if (isNewAdd) {
-                this.$emit("create", {
-                    DisplayName: data.label + "的子集",
-                    ParentId: data.parentId
-                });
                 return;
             }
-            if (data.label != "") {
-                this.$emit("rename", data.id, data.label);
-                this.renameShowId = this.curId = null;
+            else if (isNewAdd) {
+                this.$emit("create", {
+                    CategoryName: data.lable,
+                    ParentId: data.parentId
+                });
+            }else  if (data.label != "") {
+                console.log("rename");
+                this.$emit("rename", data.id, data.lable);                
             }
-            this.treeNodeId = null;
+            this.isNewAdd = isNewAdd;
+            this.renameShowId = this.curId = null;
         },
         //
         cancelhadnleTreeInput(data,node) {
          if(this.isRename){
-               if (data.label == "") {
+               if (data.lable == "") {
                 this.$message({
                     message: "分类名称不能为空",
                     type: "warning"
@@ -189,7 +134,7 @@ export default {
                 return
             }
             if(this.curlabelName != ""){
-                 data.label = this.curlabelName;
+                 data.lable = this.curlabelName;
             }
              
          }else{   
@@ -255,15 +200,14 @@ export default {
                 this.isNewAdd = true;
                 const newChild = {
                     parentId: data.id,
-                    label: "",
+                    lable: "",
                     isNewAdd: true
                 };
                 data.children.unshift(newChild);               
             }
             this.curId = null;
-            this.newAddData = data;
-            
-        },
+            this.newAddData = data;            
+        }, 
         getAllNodeIds(node, isChildNode) {
             var idList = isChildNode ? [] : [node.id];
             for (var i in node.children) {
@@ -299,9 +243,9 @@ export default {
         },
         changeCategory(data) {
             if(data.isNewAdd) return;
-            this.curlabelName = data.label;
-            this.airSearchOptions.airCategoryId = data.id;
-            //this.$emit("getPicList");
+            this.curlabelName = data.lable;
+            this.articleSearchOptions.categoryId = data.id;
+            this.$emit("getList");
             // 点击其他区域 把当前新增但未确定的节点删除掉
             this.newAddData && this.newAddData.children.shift()
             if(this.renameShowId !== data.id) this.isNewAdd = false;
