748e77a0622097652c8045d217d1714ba69900dc
zhuan xu <xuzhuan94@outlook.com>
2019-06-24 16:07:46 +0800

    内容管理页面增加各项的数量

--- userapp/src/environment/index.js
+++ userapp/src/environment/index.js
@@ -2,21 +2,22 @@
 /**
  * 开发环境的URL
  */
-let memberManageApi ='http://api.dashboard.newwezhan.cn';
+let memberManageApi = "http://localhost:8100";// 'http://api.dashboard.newwezhan.cn';
 let authorityUrl = "http://api.identity.newwezhan.cn";
-let redirectUrl = "http://localhost:8082/callback"; //"http://dashboard.newwezhan.cn/callback"
+let redirectUrl = "http://localhost:8082/callback";// "http://dashboard.newwezhan.cn/callback";
 let logoutRedirectUri = "http://dashboard.newwezhan.cn/index.html";
 let silentRedirectUri = 'http://localhost:8082/static/silent-renew.html';
 let clientId = 'dashboard.api';
 
 if (process.env.NODE_ENV === 'development') {
-    memberManageApi = 'http://api.dashboard.newwezhan.cn';
-    authorityUrl = "http://api.identity.newwezhan.cn";
-    redirectUrl =  "http://dashboard.newwezhan.cn/callback";
+    memberManageApi = "http://localhost:8100";//'http://api.dashboard.newwezhan.cn';
+    authorityUrl = "http://localhost:8000";//"http://api.identity.newwezhan.cn";
+    redirectUrl = "http://localhost:8082/callback";// "http://dashboard.newwezhan.cn/callback";
     logoutRedirectUri = "http://dashboard.newwezhan.cn/index.html";
     silentRedirectUri = 'http://localhost:8082/static/silent-renew.html';
     clientId = 'dev.dashboard.api';
 }
+
 
 /**
  * 环境的URL
