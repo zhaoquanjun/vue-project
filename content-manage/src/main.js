066f0a592cac8cf378430984445ff69d0db8471b
zhaoxiangbing <121179422@qq.com>
2019-07-03 10:09:57 +0800

    增加loading

--- content-manage/src/main.js
+++ content-manage/src/main.js
@@ -8,13 +8,10 @@ import "./styles/common.scss";
 import "./plugins/element-reset.scss"
 import "./styles/weizhan.scss";
 import "@/directive/index.js"
-import { getLocal } from "./libs/local";
 import './icons';
-import securityService from "@/services/authentication/securityService";
 Vue.config.productionTip = false;
-
-
 new Vue({
+
   router,
   store,
   render: h => h(App)
