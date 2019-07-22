4fba2d7cca864377a8b48c952c312fc94d72d0aa
woxieao <867993946@qq.comhexo scd e:/git config --global user.email 867993946@qq.com>
2019-07-22 20:05:16 +0800

    分片上传逻辑完善

---
+++ content-manage/src/api/request/chunkUploadManageApi.js
@@ -0,0 +1,26 @@
+import * as ajaxRequest from "../ajaxRequest";
+
+// case
+//GET   ajaxRequest.get(`url`,params);
+//POST  ajaxRequest.post(`url`,params);
+//PUT  ajaxRequest.put(`url`,params);
+//DELETE  ajaxRequest._delete(`url`,params);
+
+/**
+ * 获取图片列表
+ * @param {*} option 
+ */
+export const getPicList = options => {
+    return ajaxRequest.get(`/api/Picture/GetList`, options);
+}
+export const batchRemove = (isDelete, idList) => {
+    return ajaxRequest.put(`/api/Picture/ChangeDeleteStatus/${isDelete}`, idList);
+}
+
+export const changeCategory = (categoryId, idList) => {
+    return ajaxRequest.put(`/api/Picture/ChangeCategory/${categoryId}`, idList);
+}
+
+export const createFileWithoutUpload = (data) => {
+    return ajaxRequest.post(`/api/ChunkUpload/CreateFileWithoutUpload`, (data));
+}
