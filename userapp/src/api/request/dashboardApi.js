b89c7d3b5b74f4ac98e7873ea1b87ebd7eeb7af0
lisy@clouddream.net <18910332606@163.com>
2019-07-25 15:46:34 +0800

    dashboard修改

--- userapp/src/api/request/dashboardApi.js
+++ userapp/src/api/request/dashboardApi.js
@@ -1,6 +1,12 @@
 import * as ajaxRequest from "../ajaxRequest";
 
 /**
+ * 获取site列表
+ */
+export const getSites = () => {
+    return ajaxRequest.get(`/api/siteInfo/GetSites`);
+}
+/**
  * 获取app信息
  */
 export const getApplication = () => {
--- userapp/src/components/dashboard/siteinfo.vue
+++ userapp/src/components/dashboard/siteinfo.vue
@@ -1,27 +1,60 @@
 <template>
-    <div >
-        <!-- <p >123</p> -->
-        
+   <div class="content-num-section">
+        <!-- <p class="section-title">我的站点</p>
+        <el-row class="content" :gutter="16">
+            <el-col class="item" :span="24" v-for="(item, index) in siteInfo" :key="index">
+                <div class="item-box">{{item.siteName}}</div>
+            </el-col>
+        </el-row> -->
     </div>
 </template>
 
 <script>
 export default {
-    props: [""],
+    props: ["siteInfo"],
     data() {
         return {
 
         };
     },
+    
     created() {
-       
+        // this.a();
+        // console.log(this.siteInfo)
     },
     methods:{
-      
-    }
+        a(){
+            console.log(this.siteInfo)
+        }
+    },
+    mounted(){
+        this.a()
+        console.log(this.siteInfo)
+    },
 };
 </script>
 
 <style lang="scss" scoped>
-
+.content-num-section {
+    margin-top: 10px;
+    background: #fff;
+    margin-bottom: 10px;
+    .section-title {
+        padding-left: 16px;
+        padding-bottom: 16px;
+        padding-top: 14px;
+        font-size: 14px;
+        line-height: 18px;
+    }
+    .content {
+        padding: 0 24px 16px;
+        .item {
+            display: flex;
+            padding: 0 16px;
+            margin-bottom: 16px;
+            border: 1px solid black;
+            
+        }
+    }
+}
 </style>
