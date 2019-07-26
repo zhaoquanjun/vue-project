0709d855128ab0808d598ca6fb61587e0ea129c0
zhaoxiangbing <121179422@qq.com>
2019-07-26 16:44:46 +0800

    完善域名管理

---
+++ userapp/src/components/websiteManage/domain/bind-domain.vue
@@ -0,0 +1,300 @@
+<template>
+    <div class="right-pannel" :style="{width:'470px'}">
+        <div class="pannel-head">
+            <span>
+                <span>添加域名</span>
+                <el-tooltip
+                    class="item"
+                    effect="light"
+                    content="可批量为多个成员增加权限，该操作不会删除"
+                    placement="bottom-end"
+                ></el-tooltip>
+            </span>
+            <span class="close-pannel" @click="handleCancel">X</span>
+        </div>
+
+        <div class="domain-wrap">
+            <el-form @submit.native.prevent>
+                <el-form-item style="color:#262626" label="域名">
+                    <el-input v-model="domainValue" placeholder="请输入备注信息（非必填" @change="changeInput"></el-input>
+                    <div class="el-form-item__error" v-if="onerrorTip">{{onerrorText}}</div>
+                </el-form-item>
+            </el-form>
+            <div class="tips">
+                <h4 class="tips-title">温馨提示：</h4>
+                <p class="tips-item">
+                    <span>1、仅支持添加阿里云域名，非阿里云域名，请先将域名转入阿里云后再添加。</span>
+                    <span class="islink">
+                        <a
+                            href="https://help.aliyun.com/document_detail/54077.html?spm=a2c4g.11186623.2.10.61ca4c07hLv73n"
+                            target="_blank"
+                        >如何将域名转入阿里云？</a>
+                    </span>
+                </p>
+                <p class="tips-item">
+                    <span>2、系统将自动为您的域名开启CDN，可在域名列表中手动关闭。</span>
+                    <span class="islink">
+                        <a
+                            href="https://help.aliyun.com/document_detail/27101.html"
+                            target="_blank"
+                        >为什么要开启CDN？</a>
+                    </span>
+                </p>
+            </div>
+        </div>
+        <div class="pannel-footer">
+            <button class="footer-btn confirmBtn" :disabled="disabled" @click="handleConfirm">{{btnText}}</button>
+            <button class="footer-btn cancel" @click="handleCancel">取消</button>
+        </div>
+    </div>
+</template>
+<script>
+import {bindDomainAndEnableCdn} from  "@/api/request/domainApi";
+export default {
+    props:["domainAmount"],
+    data() {
+        return {
+          
+            domainValue: "",
+            onerrorTip: false,
+            onerrorText: "",
+            disabled:false,
+            btnText:"确定"
+        };
+    },
+    methods: {
+          async handleConfirm() {
+            this.disabled=true; 
+            this.btnText="添加中" 
+            if(this.domainAmount==10){
+                this.$notify({
+                    message: "每个站点最多只能绑定10个域名",
+                    type: "warning",
+                    duration:1500
+                });
+                return false;
+            }
+            if (!this.changeInput()) return;
+            let { data, status } = await bindDomainAndEnableCdn({
+                domain: this.domainValue
+            });
+            if (status === 200 && !data.isSuccess) {
+                this.onerrorTip = true;
+                this.onerrorText = data.msg;
+                return;
+            }
+            if (status === 200 && data.isSuccess) {
+                this.clickConfirmBtnSet();
+                this.$emit("getCdnDomainList"); //添加成功后刷新域名列表
+                // this.onerrorTip = false;
+                let message = [];
+                message.push(this.$createElement("p", { style: "color: #262626" },`${this.domainValue}，添加成功！可授权阿里云账号完成一键解析`));
+                message.push(this.$createElement("p",{ style: "color: #8C8C8C" },"如已存在解析记录，将会修改原有记录"));
+                this.$confirm("提示", {
+                    title: "提示",
+                    message: this.$createElement("div", null, message),
+                    confirmButtonText: "授权并一键解析",
+                    cancelButtonText: "暂不授权",
+                    type: "success",
+                    callback: async action => {
+                        console.log(action);
+                        if (action === "confirm") {
+                            this.$emit("resolveCdnByAliYunToken")
+                            this.handleCancel()
+                        } else {
+                            this.elemnetConfirm(
+                                "warning",
+                                "您可在域名列表中继续完成解析设置。",
+                                `域名未解析！`
+                            );
+                            this.handleCancel()
+                        }
+                    }
+                });
+            }
+
+            //    this.onerrorTip=true;
+            //    this.onerrorText="不好是"
+        },
+        changeInput() {
+            if (this.domainValue === "") {
+                this.onerrorTip = true;
+                this.onerrorText = "请输入域名";
+                return false;
+            } else if (!this.testDomain(this.domainValue)) {
+                this.onerrorTip = true;
+                this.onerrorText = "请输入正确格式的域名";
+                return false;
+            } else {
+                this.onerrorTip = false;
+                this.onerrorText = "";
+                return true;
+            }
+        },
+        // 提示框封装方法
+        elemnetConfirm(type, content1, content2, callback) {
+            let message = [];
+            if (content1) {
+                message.push(
+                    this.$createElement(
+                        "p",
+                        { style: "color: #262626" },
+                        content1
+                    )
+                );
+            }
+            if (content2) {
+                message.push(
+                    this.$createElement(
+                        "p",
+                        { style: "color: #8C8C8C" },
+                        content2
+                    )
+                );
+            }
+            this.$confirm("提示", {
+                title: "提示",
+                message: this.$createElement("div", null, message),
+                confirmButtonText: "确定",
+                type: type,
+                callback: async action => {
+                    if (action === "confirm") {
+                       
+                    }else{
+                       
+                    }
+                }
+            });
+        },
+        // 域名正则
+        testDomain(domain) {
+            var reg = /^([a-z0-9\-\u4E00-\u9FA5]*[\.])+([a-z\u4E00-\u9FA5]{2,10})$/;
+            return reg.test(domain);
+        },
+       
+         /**
+         * 关闭弹框
+         */
+        handleCancel() {
+             this.domainValue = "";
+             this.onerrorTip= false;
+             this.onerrorText= "";
+             this.$emit("closeDialog");
+        },
+        clickConfirmBtnSet(){
+            this.backupShow = false;
+            this.disabled=false; 
+             this.btnText="确定";
+        },
+    }
+};
+</script>
+<style scoped lang="scss">
+// 右侧弹框
+.right-pannel {
+    background: #ffffff;
+    position: fixed;
+    z-index: 2200;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    box-shadow: 0 0 3px #ccc;
+    transition: width 0.2s linear;
+    background-color: "#fff";
+    color: #262626;
+    overflow: hidden;
+    .pannel-head {
+        height: 40px;
+        line-height: 40px;
+        font-size: 14px;
+        overflow: hidden;
+        border-bottom: 1px solid #efefef;
+        span {
+            padding: 0 10px;
+        }
+        .auth-title {
+            margin-right: 10px;
+        }
+        .close-pannel {
+            float: right;
+            cursor: pointer;
+        }
+    }
+    .tips {
+        overflow: hidden;
+        margin-top: 36px;
+        padding: 10px;
+        background: rgba(242, 255, 234, 1);
+        border: 1px solid rgba(199, 221, 185, 1);
+        font-size: 12px;
+        color: rgba(0, 182, 57, 1);
+        text-align: left;
+        line-height: 17px;
+        h4,
+        p {
+            line-height: 17px;
+        }
+        .tips-title {
+            font-weight: 500;
+            padding-bottom: 8px;
+        }
+        .tips-item {
+            padding-bottom: 8px;
+        }
+    }
+    .remarkTitle {
+        font-size: 14px;
+        font-weight: 400;
+        color: rgba(38, 38, 38, 1);
+        line-height: 20px;
+        margin-left: 15px;
+    }
+    .remarkInput {
+        width: 470px;
+        margin: 15px 15px 0 15px;
+    }
+    .confirm {
+        position: absolute;
+        width: 470px;
+        height: 64px;
+        bottom: 0px;
+        border-top: 1px solid #efefef;
+        .confirmBtn {
+            margin: 16px;
+            width: 58px;
+            height: 32px;
+            background: rgba(0, 193, 222, 1);
+            font-size: 12px;
+            font-weight: 400;
+            color: rgba(255, 255, 255, 1);
+            line-height: 32px;
+        }
+    }
+}
+.domain-wrap {
+    padding: 0 16px;
+    // 绑定域名的错误提示
+    .el-form-item__error {
+        color: #262626;
+        padding-top: 8px;
+        &::before {
+            display: inline-block;
+            content: "";
+            width: 13px;
+            height: 13px;
+            vertical-align: bottom;
+            padding-right: 8px;
+            background: url("~img/jian-icon.png") no-repeat center;
+            background-size: contain;
+        }
+    }
+}
+.islink {
+    padding-top: 3px;
+    cursor: pointer;
+    display: block;
+    padding-left: 17px;
+    color: blue;
+}
+</style>
+
--- userapp/src/components/websiteManage/siteDomain.vue
+++ userapp/src/components/websiteManage/siteDomain.vue
@@ -13,19 +13,23 @@
                 <SiteInfo />
             </el-row>
             <DomainMenu @handleBtn="righPanelShow" />
-            <button @click="_oneKeyEnableHttps">开启https</button>
+            <!-- <button @click="_oneKeyEnableHttps">开启https</button>
             <button @click="_resolveCdnByAliYunToken">解析cdn</button>
             <el-button @click="_pauseCdn">暂停cdn加速</el-button>
             <el-button @click="_reopenCdn">打开cdn加速</el-button>
             <el-button @click="_removeAliYunToken">删除阿里云token</el-button>
             <el-button @click="_disableHttps">禁用https</el-button>
-            <el-button @click="_deleteCdnDomain">删除</el-button>
+            <el-button @click="_deleteCdnDomain">删除</el-button> -->
             <el-main>
                 <DomainList
                     :tableData="domainListData"
                     @_deleteCdnDomain="_deleteCdnDomain"
                     @oneKeyEnableHttps="_oneKeyEnableHttps"
                     @disableHttps="_disableHttps"
+                    @resolveCdnByAliYunToken="_resolveCdnByAliYunToken"
+                    @reopenCdn="_reopenCdn"
+                    @pauseCdn="_pauseCdn"
+                    @getCdnDomainList="_getCdnDomainList"
                 ></DomainList>
             </el-main>
         </el-main>
@@ -35,37 +39,11 @@
             :show-close="false"
             :close-on-click-modal="false"
         >
-            <div class="right-pannel" :style="{width:'470px'}">
-                <div class="pannel-head">
-                    <span>
-                        <span>备份当前版本</span>
-                        <el-tooltip
-                            class="item"
-                            effect="light"
-                            content="可批量为多个成员增加权限，该操作不会删除"
-                            placement="bottom-end"
-                        ></el-tooltip>
-                    </span>
-                    <span class="close-pannel" @click="closeDialog">X</span>
-                </div>
-                <div class="tips">温馨提示：备份当前站点设计页面，包括电脑版、手机版和Pad版</div>
-                <div class="domain-wrap">
-                    <el-form>
-                        <el-form-item label="域名">
-                            <el-input
-                                v-model="domainValue"
-                                placeholder="请输入备注信息（非必填"
-                                @change="changeInput"
-                            ></el-input>
-                            <div class="el-form-item__error" v-if="onerrorTip">{{onerrorText}}</div>
-                        </el-form-item>
-                    </el-form>
-                </div>
-                <div class="pannel-footer">
-                    <button class="footer-btn confirmBtn" @click="handleConfirm">确定</button>
-                    <button class="footer-btn cancel" @click="handleCancel">取消</button>
-                </div>
-            </div>
+            <BindDomain
+                :domain-amount="domainAmount"
+                @closeDialog="closeDialog"
+                @resolveCdnByAliYunToken="_resolveCdnByAliYunToken"
+            ></BindDomain>
         </el-dialog>
     </el-container>
 </template>
@@ -76,6 +54,7 @@ import PageSubmenu from "@/components/common/PageSubmenu";
 import SiteInfo from "_c/websiteManage/domain/site-info.vue";
 import DomainMenu from "_c/websiteManage/domain/domain-menu.vue";
 import DomainList from "_c/websiteManage/domain/domain-list.vue";
+import BindDomain from "_c/websiteManage/domain/bind-domain.vue";
 import { sendTargetPhoneCode } from "@/api/index.js";
 import { formatDateTime } from "@/api/index";
 import * as domainApi from "@/api/request/domainApi";
@@ -86,19 +65,10 @@ export default {
         PageSubmenu,
         SiteInfo,
         DomainMenu,
-        DomainList
+        DomainList,
+        BindDomain
     },
     data() {
-        // var checkDomain = (rule, value, callback) => {
-        //     var reg = /^((?!-)[A-Za-z0-9-\u4E00-\u9FA5]{1,63}(?<!-)\.)+[A-Za-z]{2,6}$/;
-        //     if (value === "") {
-        //         callback(new Error("请输入域名"));
-        //     } else if (!reg.test(value)) {
-        //         callback(new Error("请输入正确格式的域名"));
-        //     } else {
-        //         callback();
-        //     }
-        // };
         return {
             submenuList: [
                 { name: "网站备份", url: "/website/backup" },
@@ -107,6 +77,7 @@ export default {
                 { name: "域名管理", url: "/website/sitedomain" },
                 { name: "邮件服务器", url: "/website/email" }
             ],
+            domainListData: [], // table列表
             siteInfoImg: "",
             siteName: "",
             siteId: 0,
@@ -115,31 +86,17 @@ export default {
             siteInfo: [],
             manualSite: [],
             autoSite: [],
-
             backupShow: false,
             backuping: false,
             // recovery: false,
             remarkInfo: "",
-            // /////////////
-            // domainDetail: {
-            //     curDomain: ""
-            // },
-            // rules: {
-            //     curDomain: [
-            //         // { validator: checkDomain, trigger: "blur", require: true }
-            //     ]
-            // },
-
-            domainListData: [],
-            domainValue: "",
-            onerrorTip: false,
-            onerrorText: ""
+            domainAmount:0,
         };
     },
-    created(){
-        window.callback = function(){
-            console.log("重新打开窗口")
-        }
+    created() {
+        window.callback = function() {
+            console.log("重新打开窗口");
+        };
     },
     mounted() {
         this._getCdnDomainList();
@@ -147,8 +104,8 @@ export default {
         this._removeAliYunToken();
     },
     methods: {
-        async _removeAliYunToken(){
-            await domainApi.removeAliYunToken()
+        async _removeAliYunToken() {
+            await domainApi.removeAliYunToken();
         },
         /**
          * 获取站点信息
@@ -167,79 +124,40 @@ export default {
         async _getCdnDomainList() {
             let { data } = await domainApi.getCdnDomainList();
             this.domainListData = data;
-            console.log(data);
+            this.domainAmount = data.length;
         },
-        async handleConfirm() {
-             
-            if (!this.changeInput()) return;
-            let { data, status } = await domainApi.bindDomainAndEnableCdn({
-                domain: this.domainValue
-            });
-            if (status === 200 && !data.isSuccess) {
-                this.onerrorTip = true;
-                this.onerrorText = data.msg;
-                return;
-            }
-            if (status === 200 && data.isSuccess) {
-                // this.onerrorTip = false;
-                let message = [];
-                message.push(this.$createElement('p',{ style: 'color: #262626'}, `${this.domainValue}，添加成功！可授权阿里云账号完成一键解析`))
-                message.push(this.$createElement('p',  { style: 'color: #8C8C8C'}, "如已存在解析记录，将会修改原有记录"))
-                this.$confirm(
-                    "提示",
-                    {
-                         message: this.$createElement('div', null, message),
-                        confirmButtonText: "授权并一键解析",
-                        cancelButtonText: "暂不授权",
-                        type: "success",
-                        callback: async action => {
-                            console.log(action);
-                            if (action === "confirm") {
-                                  this._resolveCdnByAliYunToken()  
-                            }else{
-                                this.elemnetConfirm("warning","您可在域名列表中继续完成解析设置。",`域名未解析！`)
-                            }
-                        }
-                    }
-                );
-            }
 
-            //    this.onerrorTip=true;
-            //    this.onerrorText="不好是"
-        },
-        submitForm(formName, imageUrl) {
-            this.$refs.domain.validate(valid => {
-                if (valid) {
-                    this.handleConfirm();
-                } else {
-                    console.log("error submit!!");
-                    return false;
-                }
-            });
-        },
         /**
          * 解析域名
          */
-        async _resolveCdnByAliYunToken() {
-            let params = {
-                siteId: 2,
+        async _resolveCdnByAliYunToken(
+            params = {
+                siteId: this.$store.state.dashboard.siteId,
                 resolveType: "",
                 domain: this.domainValue,
                 resolveValue: "",
                 isForceUpdate: false
-            };
-            let {data} = await domainApi.resolveCdnByAliYunToken(params);
-            if(!data.isSuccess && data.redirectUrl){
-                 window.open(data.redirectUrl)
             }
-           
+        ) {
+            let { data } = await domainApi.resolveCdnByAliYunToken(params);
+            if (!data.isSuccess && data.redirectUrl) {
+                window.open(data.redirectUrl);
+            }
         },
         /**
          * 开启https
          */
         async _oneKeyEnableHttps(domainId) {
-            alert(domainId);
-            let data = await domainApi.oneKeyEnableHttps(domainId);
+            let { data, status } = await domainApi.oneKeyEnableHttps(domainId);
+            if (status === 200) {
+                // 
+                this.$notify({
+                    title: "成功",
+                    message: "证书申请成功！",
+                    type: "success",
+                    duration:1500
+                });
+            }
         },
         /**
          * 关闭https
@@ -248,14 +166,32 @@ export default {
             let data = await domainApi.disableHttps(domainId);
             console.log(data);
         },
-
-        async _pauseCdn() {
-            let data = await domainApi.pauseCdn(4);
-            console.log(data);
+        /**
+         * 关闭cnd加速
+         */
+        async _pauseCdn(siteDomainId) {
+            let {data,status} = await domainApi.pauseCdn(siteDomainId);
+            if(status===200){
+                  this.$notify({
+                    message: "已成功关闭CDN",
+                    type: "success",
+                    duration:1500
+                });
+                this._getCdnDomainList();
+            }
+            
         },
-        async _reopenCdn() {
-            let data = await domainApi.reopenCdn(4);
-            console.log(data);
+        /** 开启cdn加速 */
+        async _reopenCdn(siteDomainId) {
+            let data = await domainApi.reopenCdn(siteDomainId);
+            if(status===200){
+                  this.$notify({
+                    message: "已开启CDN，等待服务器审核",
+                    type: "success",
+                    duration:1500
+                });
+                this._getCdnDomainList();
+            }
         },
         async _removeAliYunToken() {
             let data = await domainApi.removeAliYunToken();
@@ -279,6 +215,7 @@ export default {
                         );
                         if (status === 200) {
                             this.domainListData.splice(index, 1);
+                            this.domainAmount--;
                         }
                     }
                 }
@@ -287,55 +224,9 @@ export default {
         righPanelShow() {
             this.backupShow = true;
         },
-        /**
-         * 关闭弹框
-         */
-        handleCancel() {
-            this.backupShow = false;
-        },
+
         closeDialog() {
             this.backupShow = false;
-        },
-        testDomain(domain) {
-            var reg = /^([a-z0-9\-\u4E00-\u9FA5]*[\.])+([a-z\u4E00-\u9FA5]{2,10})$/;
-            return reg.test(domain);
-        },
-        changeInput() {
-            if (this.domainValue === "") {
-                this.onerrorTip = true;
-                this.onerrorText = "请输入域名";
-                return false;
-            } else if (!this.testDomain(this.domainValue)) {
-                this.onerrorTip = true;
-                this.onerrorText = "请输入正确格式的域名";
-                return false;
-            } else {
-                this.onerrorTip = false;
-                this.onerrorText = "";
-                return true;
-            }
-        },
-        elemnetConfirm(type,content1,content2,callback) {
-             let message = [];
-                if(content1){
-                     message.push(this.$createElement('p',{ style: 'color: #262626'}, content1))
-                }
-                if(content2){
-                     message.push(this.$createElement('p',  { style: 'color: #8C8C8C'},content2 ))
-                }
-                this.$confirm(
-                    "提示",
-                    {
-                         message: this.$createElement('div', null, message),
-                        confirmButtonText: "确定",
-                        type: type,
-                        callback: async action => {
-                            if (action === "confirm") {
-                                
-                            }
-                        }
-                    }
-                );
         }
     }
 };
@@ -344,26 +235,6 @@ export default {
 <style scoped>
 .el-dialog {
     right: 0;
-}
-
-.el-table /deep/ thead th {
-    padding: 0;
-    height: 35px;
-    background: #00c1de !important;
-    color: #fff;
-    font-weight: 400;
-    font-size: 12px;
-}
-.content-table {
-    width: 100%;
-    box-sizing: border-box;
-}
-.remark-desc {
-    display: inline-block;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    width: 81%;
 }
 </style>
 
@@ -415,97 +286,5 @@ export default {
             background-size: 100%;
         }
     }
-}
-// 右侧弹框
-.right-pannel {
-    background: #ffffff;
-    position: fixed;
-    z-index: 2200;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    box-shadow: 0 0 3px #ccc;
-    transition: width 0.2s linear;
-    background-color: "#fff";
-    color: #262626;
-    overflow: hidden;
-    .pannel-head {
-        height: 40px;
-        line-height: 40px;
-        font-size: 14px;
-        overflow: hidden;
-        border-bottom: 1px solid #efefef;
-        span {
-            padding: 0 10px;
-        }
-        .auth-title {
-            margin-right: 10px;
-        }
-        .close-pannel {
-            float: right;
-            cursor: pointer;
-        }
-    }
-    .tips {
-        width: 440px;
-        height: 32px;
-        background: rgba(242, 255, 234, 1);
-        border: 1px solid rgba(199, 221, 185, 1);
-        font-size: 12px;
-        font-weight: 500;
-        color: rgba(0, 182, 57, 1);
-        line-height: 32px;
-        text-align: center;
-        margin: 15px;
-    }
-    .remarkTitle {
-        font-size: 14px;
-        font-weight: 400;
-        color: rgba(38, 38, 38, 1);
-        line-height: 20px;
-        margin-left: 15px;
-    }
-    .remarkInput {
-        width: 470px;
-        margin: 15px 15px 0 15px;
-    }
-    .confirm {
-        position: absolute;
-        width: 470px;
-        height: 64px;
-        bottom: 0px;
-        border-top: 1px solid #efefef;
-        .confirmBtn {
-            margin: 16px;
-            width: 58px;
-            height: 32px;
-            background: rgba(0, 193, 222, 1);
-            font-size: 12px;
-            font-weight: 400;
-            color: rgba(255, 255, 255, 1);
-            line-height: 32px;
-        }
-    }
-}
-</style>
-<style scoped lang="scss">
-.el-form-item__error {
-    color: #262626;
-    &::before {
-        display: inline-block;
-        content: "";
-        width: 13px;
-        height: 13px;
-        vertical-align: bottom;
-        padding-right: 8px;
-        background: url("~img/jian-icon.png") no-repeat center;
-        background-size: contain;
-    }
-}
-.domain-wrap {
-    padding: 0 16px;
-}
-.el-form-item.is-error {
-    border-color: #f56c6c;
 }
 </style>
