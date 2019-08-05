700088eca389e42e987c8b6186e1163e32c6ef59
duhan <18910332606@163.com>
2019-08-05 12:00:05 +0800

    dashboard修改

--- userapp/src/components/dashboard/plugins.vue
+++ userapp/src/components/dashboard/plugins.vue
@@ -1,156 +1,107 @@
 <template>
-    <el-row class="plugin-section" :gutter="16">
-        <el-col class="plugins-box" :span="12" v-for="(it, idx) in  plugins" :key="idx">
-            <div class="box-inner">
-                <span class="plugin-l plugin-item" :class="it.pluginCode"></span>
-                <span class="plugin-r plugin-item">
-                    <h2>{{it.name}}</h2>
-                    <p>{{it.description}}</p>
-                    <p>{{it.isActive ? '激活' : '未激活'}}</p>
-                    <div :class="pluginsPic(it)" class="backgroundImg"></div>
-                </span>
-            </div>
-        </el-col>
-       
-    </el-row>
+  <el-row class="plugin-section" :gutter="40">
+    <el-col class="plugins-box" :span="6" v-for="(it, idx) in  plugins" :key="idx">
+      <div class="box-inner">
+        <span class="plugin-item plugin-img" :class="it.pluginCode"></span>
+        <span class="plugin-item plugin-text">
+          <h2>{{it.name}}</h2>
+          <p>{{it.description}}</p>
+          <p>{{it.isActive ? '激活' : '未激活'}}</p>
+        </span>
+      </div>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 export default {
-    props: ["plugins"],
-    data() {
-        return {
-            clonePlugins: []
-        };
-    },
-    methods: {
-        pluginsPic(it) {
-            switch (it.pluginCode) {
-                case "MicroProgram":
-                    return "MicroProgramGround";
-                case "AliPayProgram":
-                    return "AliPayProgramGround";
-                case "OfficialAccounts":
-                    return "OfficialAccountsGround";
-                case "Form":
-                    return "FormGround";
-            }
-        }
-    },
-    computed: {}
+  props: ["plugins"]
 };
 </script>
 
 <style lang="scss" scoped>
 body {
-    background: #f5f5f6;
+  background: #f5f5f6;
 }
 .plugin-section {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  flex-wrap: wrap;
+  .plugins-box {
     display: flex;
-    justify-content: space-between;
-    align-content: center;
-    flex-wrap: wrap;
-    .plugins-box {
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-        //padding-left: 48px;
-        margin-bottom: 16px;
-        cursor: pointer;
-        .box-inner {
-            width: 100%;
-            display: flex;
-            position: relative;
-            justify-content: flex-start;
-            align-items: center;
-            background: #fff;
-            padding: 30px 50px;
-            padding-right: 10px;
-            border: 1px solid transparent;
-            &:hover {
-                border: 1px solid #00c1de;
-            }
-            .plugin-item {
-                display: block;
-            }
-            .plugin-l {
-                width: 48px;
-                height: 48px;
-            }
-            .MicroProgram {
-                background: url("~img/dashboard/board-wxprogram.png") no-repeat
-                    center;
-                background-size: contain;
-            }
-            .AliPayProgram {
-                background: url("~img/dashboard/board-alipay.png") no-repeat
-                    center;
-                background-size: contain;
-            }
-            .OfficialAccounts {
-                background: url("~img/dashboard/board-wxpublicNumber.png")
-                    no-repeat center;
-                background-size: contain;
-            }
-            .Form {
-                background: url("~img/dashboard/board-form.png") no-repeat
-                    center;
-                background-size: contain;
-            }
-            .backgroundImg {
-                position: absolute;
-                right: 0px;
-                bottom: 0px;
-            }
-            .MicroProgramGround {
-                width: 132px;
-                height: 118px;
-                background: url("~img/dashboard/board-pluginImg1.png") no-repeat;
-                background-position: right bottom;
-                background-size: contain;
-            }
-            .AliPayProgramGround {
-                width: 131px;
-                height: 127px;
-                background: url("~img/dashboard/board-pluginImg2.png") no-repeat;
-                background-position: right bottom;
-                background-size: contain;
-            }
-            .OfficialAccountsGround {
-                width: 108px;
-                height: 122px;
-                background: url("~img/dashboard/board-pluginImg3.png") no-repeat;
-                background-position: right bottom;
-                background-size: contain;
-            }
-            .FormGround {
-                width: 132px;
-                height: 107px;
-                background: url("~img/dashboard/board-pluginImg4.png") no-repeat;
-                background-position: right bottom;
-                background-size: contain;
-            }
-            .plugin-r {
-                margin-left: 40px;
-                h2 {
-                    font-size: 18px;
-                    line-height: 25px;
-                    // padding-top: 30px;
-                }
-                p:first-of-type {
-                    font-size: 12px;
-                    padding-top: 4px;
-                    line-height: 17px;
-                    color: #8c8c8c;
-                }
-                p:last-of-type {
-                    font-size: 12px;
-                    padding-top: 18px;
-                    line-height: 17px;
-                    color: #00c1de;
-                }
-            }
+    justify-content: flex-start;
+    align-items: center;
+    margin-bottom: 42px;
+    cursor: pointer;
+    .box-inner {
+      width: 100%;
+      position: relative;
+      background: #fff;
+      border: 1px solid transparent;
+      border-radius: 3px;
+      border: 1px solid rgba(229, 229, 229, 1);
+      text-align: center;
+
+      background: url("~img/dashboard/board-pluginImg.png");
+      background-repeat: no-repeat;
+      background-position: right bottom;
+      background-size: 143px 131px;
+      &:hover {
+        border: 1px solid #00c1de;
+      }
+      .plugin-item {
+        display: block;
+      }
+      .plugin-img {
+        width: 84px;
+        height: 84px;
+        margin: 20px auto 0px;
+      }
+      .MicroProgram {
+        background: url("~img/dashboard/board-wxprogram.png") no-repeat center;
+        background-size: contain;
+      }
+      .AliPayProgram {
+        background: url("~img/dashboard/board-alipay.png") no-repeat center;
+        background-size: contain;
+      }
+      .OfficialAccounts {
+        background: url("~img/dashboard/board-wxpublicNumber.png") no-repeat
+          center;
+        background-size: contain;
+      }
+      .Form {
+        background: url("~img/dashboard/board-form.png") no-repeat center;
+        background-size: contain;
+      }
+      .plugin-text {
+        h2 {
+          font-size: 18px;
+          font-family: PingFangSC-Medium;
+          font-weight: 500;
+          color: rgba(38, 38, 38, 1);
+          line-height: 25px;
         }
+        p:first-of-type {
+          font-size: 14px;
+          font-family: PingFangSC-Regular;
+          font-weight: 400;
+          color: rgba(161, 168, 177, 1);
+          line-height: 20px;
+          margin-top: 8px;
+        }
+        p:last-of-type {
+          font-size: 14px;
+          font-family: PingFangSC-Regular;
+          font-weight: 400;
+          color: rgba(9, 204, 235, 1);
+          line-height: 20px;
+          margin-top: 24px;
+          margin-bottom: 36px;
+        }
+      }
     }
+  }
 }
 </style>
