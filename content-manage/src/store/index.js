38d6099c25d20b1bf1d0324c4d93c5d489d51602
duhan <18910332606@163.com>
2019-08-08 20:34:29 +0800

    contentHeader修改

--- content-manage/src/store/index.js
+++ content-manage/src/store/index.js
@@ -60,6 +60,6 @@ if(getLocal("appid")){
 if(Cookies("routeCode")){
   store.commit("SETCODE",Cookies("routeCode"))
 }
-if(sessionStorage.getItem("userInfo")){
+if(Cookies("userInfo")){
   store.commit("SET_USERINFO",JSON.parse(getLocal("userInfo")))
 }
