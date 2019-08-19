cd6d1a34ca935700efae1711d76ef5b1a36ac10d
duhan <18910332606@163.com>
2019-08-19 17:21:54 +0800

    整站模版

--- template-manage/src/utlis/index.js
+++ template-manage/src/utlis/index.js
@@ -1,4 +1,4 @@
-let languageList={ "zh-CN":"简体中文", "en-US": "English", "ja-JP": "日本语", "es-ES": "Español", "ko-KR": "한국어" };
+let languageList={ "zh-CN":"中文", "en-US": "英文", "ja-JP": "日语", "es-ES": "西班牙语", "ko-KR": "韩语" };
 export const getLanguage = (language) =>{
     for (var x in languageList) {
         if (language == x) {
