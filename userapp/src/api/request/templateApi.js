d1463d85bdb595b4947605850a5e258f7d0d08a2
duhan <18910332606@163.com>
2019-08-27 10:49:01 +0800

    选择模版

--- userapp/src/api/request/templateApi.js
+++ userapp/src/api/request/templateApi.js
@@ -1,9 +1,13 @@
 import * as ajaxRequest from "../ajaxRequest";
 import environment from "@/environment/index";
 
-// 获取模版列表
+// 获取全部模版列表
 export const getSiteTemplates = (para) => {
     return ajaxRequest.get(`${environment.siteManageApi}/api/siteInfo/GetSiteTemplates`, para);
+}
+// 获取已有网站模版列表
+export const getTemplateSites = () => {
+    return ajaxRequest.get(`${environment.siteManageApi}/api/siteInfo/GetTemplateSites`);
 }
 // 获取行业tree
 export const getIndustryTree = () => {
@@ -12,4 +16,8 @@ export const getIndustryTree = () => {
 // 选择模版
 export const updateSiteTemplate = (para) => {
     return ajaxRequest.put(`${environment.siteManageApi}/api/siteInfo/UpdateSiteTemplate`, para);
+}
+// 未找到想要的模版
+export const createReferenceIndustry = (para) => {
+    return ajaxRequest.post(`${environment.siteManageApi}/api/siteInfo/CreateReferenceIndustry`, para);
 }
