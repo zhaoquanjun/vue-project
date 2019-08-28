c42ac4c23db89fbf3105564834d54051f2b56a39
zhaoxiangbing <121179422@qq.com>
2019-08-28 15:04:18 +0800

    暂存

---
+++ wechat-public-account/src/components/wechat-account/auto-answer/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <el-row class="user-list">
+            <span class="member-list-title fs14">域名管理</span>
+        </el-row>
+    </div>
+</template>
+<style lang="scss" scoped>
+.user-list {
+    border-bottom: 1px solid #eee;
+    padding-bottom: 10px;
+    .member-list-title {
+        border-left: 2px solid #01c0de;
+        padding-left: 10px;
+    }
+}
+</style>
