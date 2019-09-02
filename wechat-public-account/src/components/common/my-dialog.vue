79fa35c6b7737725c2723ca04073187588cf6ae1
zhaoxiangbing <121179422@qq.com>
2019-09-02 16:18:04 +0800

    修改左侧菜单---重要

---
+++ wechat-public-account/src/components/common/my-dialog.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="my-dialog" v-if="isShow" @click.stop="handlerClick"></div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            
+        };
+    },
+    methods: {
+        handlerClick() {
+            this.$store.commit("SET_DIALOG",false)
+        }
+    },
+    computed:{
+        isShow(){
+            return this.$store.state.isAsideDialogShow
+        }
+    }
+};
+</script>
+<style lang="scss" scoped>
+.my-dialog {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0.5;
+    z-index: 101;
+    background: #000;
+}
+</style>
