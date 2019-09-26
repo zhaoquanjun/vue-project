c94be36885b68755d14db5031d494e722ccc1eed
kevin <740172136@qq.com>
2019-09-26 10:24:33 +0800

    微信推广

--- wechat-public-account/src/views/wechat-account/wechatspread.vue
+++ wechat-public-account/src/views/wechat-account/wechatspread.vue
@@ -1,20 +1,51 @@
 <template>
-    <div class="account-setting__section">
-        <page-sub-nav :title="title"></page-sub-nav>
-        <div class="account-setting__manage" :style="{height:scrollHeight+'px'}" v-scrollBar>
-        </div>
-    </div> 
+  <div class="account-setting__section">
+    <div class="account-setting__manage">
+      <ChangeSite
+        @chooseWebsite="chooseWebsite"
+        @getSiteId="getSiteId"
+      />
+     </div>
+     <div>微信推广</div>
+  </div>
 </template>
+
 <script>
-import PageSubNav from "_c/common/WechatTitle";
+import ChangeSite from "@/components/common/changeSite";
+import { unBind } from "@/api/request/account.js";
+import { notify } from "@/utlis/index.js";
 export default {
-    data() {
-        return {
-            title: "账号设置",
-        }
+  data() {
+    return {
+      siteId: this.$store.state.dashboard.siteId
+    };
+  },
+  components: {
+    ChangeSite
+  },
+  created() {
+    this._getWxIsAuth();
+  },
+  methods: {
+    getSiteId(siteId) {
+      console.log('000')
     },
-    components: {
-        PageSubNav
+    // 切换站点刷新信息
+    chooseWebsite(siteId) {
+      this._getWxIsAuth()
+    },
+    // 校验是否已经授权认证
+    async _getWxIsAuth() {
+      await this.$store.dispatch('_getWxStatus')
+      let wx_status = this.$store.state.wxaccount.wx_status
+      console.log(this.$store.state.wxaccount)
+      if (!wx_status.isAuth || !wx_status.isCertification) {
+        this.$router.replace({path:'/wechataccount/wxauther' });
+      }
+      this.accountInfo = this.$store.state.wxaccount.account_info
     }
-}
+  }
+};
 </script>
+<style lang="scss" scoped>
+</style>
