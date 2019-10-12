fe1ae356645550b458134bb66d126ee1072114ff
kevin <740172136@qq.com>
2019-10-12 15:53:06 +0800

    weixintuiguang

--- wechat-public-account/src/components/wechat-account/spread/statistics.vue
+++ wechat-public-account/src/components/wechat-account/spread/statistics.vue
@@ -23,10 +23,12 @@
           <span>230</span>
         </div>
       </div>
+      <div class="btn">
+      </div>
+      <div id="myChart" :style="{width: '100%', height: '300px'}">
+
+      </div>
     </div>
-    <div class="btn">
-    </div>
-    <div id="myChart" :style="{width: '100%', height: '300px'}"></div>
   </div>
 </template>
 
@@ -37,7 +39,7 @@ let echarts = require('echarts/lib/echarts')
 // 引入柱状图组件
 require('echarts/lib/chart/line')
 // 引入提示框和title组件
-require('echarts/lib/component/tooltip')
+//require('echarts/lib/component/tooltip')
 require('echarts/lib/component/legend')
 export default {
   props: {
@@ -138,6 +140,52 @@ export default {
       text-align: center;
     }
   }
-  
+  .body {
+    padding: 0 32px;
+    height:570px;
+    background:rgba(255,255,255,1);
+    border-radius:4px;
+    border:1px solid rgba(229,229,229,1);
+    h4 {
+      font-size:14px;
+      font-weight:400;
+      color:rgba(38,38,38,1);
+      line-height:22px;
+      margin: 24px 0 16px;
+    }
+    .total {
+      display: flex;
+      div {
+        height: 85px;
+        padding: 8px;
+        flex: 1;
+        height:85px;
+        border-radius:4px;
+        border:1px solid rgba(229,229,229,1);
+        margin-right: 24px;
+        &.div:last-child {
+          margin-right: 0;
+        }
+        img {
+          float: left;
+          width: 70px;
+          height: 70px;
+          margin-right: 16px;
+        }
+        p {
+          font-weight:400;
+          color:rgba(161,168,177,1);
+          line-height:32px;
+          margin-top: 6px;
+        }
+        span {
+          font-size:24px;
+          font-family:"HelveticaNeue";
+          color:rgba(38,38,38,1);
+          line-height:24px;
+        }
+      }
+    }
+  }
 }
 </style>
