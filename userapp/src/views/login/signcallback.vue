eb22ee44146e76d238ecfc151c9a58bfd9250eae
zxb <121179422@qq.com>
2019-10-17 12:46:59 +0800

    修改登录

--- userapp/src/views/login/signcallback.vue
+++ userapp/src/views/login/signcallback.vue
@@ -16,7 +16,8 @@ export default {
                 returnToUrl = result.state;
             }
             await store.dispatch("_set", result)
-            window.location =window.location.origin + returnToUrl;
+             store.commit("SET_USER",accessToken)
+            window.location = window.location.origin + returnToUrl;
         } catch (e) {
             console.log(e,'errrrrr')
         }
