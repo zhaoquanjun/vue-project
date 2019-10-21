907ee2c04a9f674b274c16027ed28bbce397de66
kevin <740172136@qq.com>
2019-10-21 18:15:39 +0800

    侧边栏修改

--- content-manage/src/components/common/PageSubmenu.vue
+++ content-manage/src/components/common/PageSubmenu.vue
@@ -1,105 +1,134 @@
 <template>
-    <div class="submenu">
-        <h4 class="submenu-title">
-             <i>{{subTitle}}</i>
-        </h4>
-        <ul class="submenu-list">
-            <li
-                @click="handlerRoute(item,index)"
-                v-for="(item,index) in menuList"
-                :key="index"
-                :class="{'active':lastRoute==item.code}"
-            >{{item.name}}</li>
-        </ul>
-    </div>
+  <div class="submenu">
+    <h4 class="submenu-title">
+      <i>{{subTitle}}</i>
+    </h4>
+    <ul class="submenu-list">
+      <li
+        @click="handlerRoute(item,index)"
+        v-for="(item,index) in menuList"
+        :key="index"
+        :class="{'active':lastRoute == item.code}"
+      >
+      <span>{{item.name}}</span>
+        <div v-show="item.children">
+          <p 
+            v-for="(item2,ind) in item.children"
+            @click.stop="handlerRoute(item2,index)"
+            :key="ind"
+            :class="{'active':lastRoute== item2.code}"
+          >
+            {{item2.name}}
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 import { siteDomain } from "@/environment/index";
 export default {
-    data() {
-        return {
-            children: "",
-            lastRoute: "",
-            parentPath: "",
-            subTitle:""
+  data() {
+    return {
+      children: [],
+      lastRoute: "",
+      parentPath: "",
+      subTitle: ""
+    };
+  },
 
-        };
+  methods: {
+    handlerRoute(item, index) {
+      //有三级路由时，二级路由不跳转
+      if (item.children && item.children.length > 0) {
+        return
+      }
+      let domain = item.menuUrl.split("/")[0];
+      if (siteDomain == domain) {
+        this.$router.push(item.path);
+        return;
+      } else {
+        window.location.href = "//" + item.menuUrl;
+      }
     },
-
-    methods: {
-        handlerRoute(item, index) {
-            let domain = item.menuUrl.split("/")[0];
-            if (siteDomain == domain) {
-                this.$router.push(item.path);
-                return
-            } else {
-                window.location.href = "//" + item.menuUrl;
-            }
-             
-        },
-        getChildrenMenuList(curPath) {
-            this.menuList.forEach(item => {
-                if (curPath == item.code) {
-                    this.children = item.children;
-                }
-            });
+    
+  },
+  mounted() {
+    let routerList = this.$route.path.split("/");
+    let [, firstRoute, lastRoute] = this.$route.path.split("/");
+    this.lastRoute = routerList[routerList.length-1];
+    this.parentPath = firstRoute;
+  },
+  computed: {
+    menuList() {
+      let menuList1 = this.$store.state.dashboard.menuList;
+      let children = [];
+      menuList1.map(item => {
+        if (this.parentPath == item.code) {
+          this.subTitle = item.name;
+          children = item.children;
         }
-    },
-    computed: {
-        menuList() {
-            let menuList1 = this.$store.state.dashboard.menuList;
-            let children = [];
-            menuList1.map(item => {
-                if (this.parentPath == item.code) {
-                     this.subTitle=item.name
-                    children = item.children;
-                }
-            });
-            return children;
-        }
-    },
-    watch: {
-        $route(to, from) {
-         
-            let [, firstRoute, lastRoute] = this.$route.path.split("/");
-            this.lastRoute = lastRoute;
-            this.parentPath = firstRoute;
-        }
+      });
+      return children;
     }
+  },
+  watch: {
+    $route(to, from) {
+      let routerList = this.$route.path.split("/");
+      let [, firstRoute, lastRoute] = this.$route.path.split("/");
+      this.lastRoute = routerList[routerList.length-1];
+      this.parentPath = firstRoute;
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
 .submenu {
-    max-width: 100px;
-    height: calc(100vh - 60px);
+    width: 150px;
+    height: calc(100vh - 50px);
     // border-right: 1px solid #e5e5e5;
      background: #F8FAFC;
     .submenu-title {
         height: 40px;
         line-height: 40px;
-        // padding-left: 12px;
+        padding-left: 24px;
         padding-top: 5px;
-        text-align: center;
+        text-align: left;
     }
     .submenu-list {
         padding-top: 7px;
         li {
+          cursor: pointer;
+          line-height: 50px;
+          font-size: 14px;
+          font-weight:400;
+          color:rgba(38,38,38,1);
+          span {
+            padding-left: 24px;
             cursor: pointer;
-            height: 50px;
+          }
+          p {
+            cursor: pointer;
+            padding-left: 40px;
             line-height: 50px;
-            // padding-left: 12px;
-            margin-bottom: 10px;
-            text-align: center;
+            font-size: 14px;
+            font-weight:400;
+            color:rgba(38,38,38,1);
             &:hover {
               color: #262626;
-               background: #fff;
+              background: #fff;
             }
+            .active {
+              background: #fff !important;
+              color: #0595e6 !important;
+            }
+          }
         }
     }
 }
 .active {
-    background: #fff;
-     color: #0595e6;
+    background: #fff !important;
+    color: #0595e6 !important;
 }
 .active:hover {
     background: #fff ;
