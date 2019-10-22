6ee8e7007ca07c07696a45ba4ac8d999c89e923b
kevin <740172136@qq.com>
2019-10-22 15:18:46 +0800

    内容管理wezhan.cn改为clouddream.net

--- content-manage/src/environment/index.js
+++ content-manage/src/environment/index.js
@@ -6,21 +6,21 @@
 
 /**生产环境-- */
 
-const httpSchema = `http://`;
-let contentApi = `${httpSchema}api.content.console.wezhan.cn`;
-let dashboardApi = `${httpSchema}api.dashboard.console.wezhan.cn`;
-let uploadPicUrl = `${httpSchema}api.content.console.wezhan.cn/api/picture`;
-let authorityUrl = `${httpSchema}login.console.wezhan.cn`;
-let redirectUrl = `${httpSchema}content.console.wezhan.cn/callback`;
-let logoutRedirectUri = `${httpSchema}content.console.wezhan.cn/index.html`;
-let silentRedirectUri = `${httpSchema}content.console.wezhan.cn/static/silent-renew.html`;
-let designerManageApi = `${httpSchema}api.designer.console.wezhan.cn`;
+const httpSchema = `https://`;
+let contentApi = `${httpSchema}api.content.console.clouddream.net`;
+let dashboardApi = `${httpSchema}api.dashboard.console.clouddream.net`;
+let uploadPicUrl = `${httpSchema}api.content.console.clouddream.net/api/picture`;
+let authorityUrl = `${httpSchema}login.console.clouddream.net`;
+let redirectUrl = `${httpSchema}content.console.clouddream.net/callback`;
+let logoutRedirectUri = `${httpSchema}content.console.clouddream.net/index.html`;
+let silentRedirectUri = `${httpSchema}content.console.clouddream.net/static/silent-renew.html`;
+let designerManageApi = `${httpSchema}api.designer.console.clouddream.net`;
 let clientId = 'content.api';
 let scope = 'openid profile dashboard.api content.api designer.api produce.api weixinopen.api';
-export let siteDomain = 'content.console.wezhan.cn';
+export let siteDomain = 'content.console.clouddream.net';
 // 个人中心
-export let personalUrl = '//dashboard.console.wezhan.cn/personal';
-export let dashboardUrl = `${httpSchema}dashboard.console.wezhan.cn/board`;
+export let personalUrl = '//dashboard.console.clouddream.net/personal';
+export let dashboardUrl = `${httpSchema}dashboard.console.clouddream.net/board`;
 // 阿里云续费地址
 export let aliMarketUrl = 'https://market.console.aliyun.com/imageconsole/index.htm?#/?_k=dd45g0';
 /**
