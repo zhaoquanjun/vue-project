a8259eaa2e824db939c911acd3979d85485cefcc
kevin <740172136@qq.com>
2019-10-22 11:47:26 +0800

    微信公众号，侧边栏修改

--- wechat-public-account/src/components/Aside/LeftNavComponents.vue
+++ wechat-public-account/src/components/Aside/LeftNavComponents.vue
@@ -5,49 +5,82 @@
         </h4>
         <ul class="el-menu-vertical-demo el-menu">
             <li
-                class="el-menu-item"
+                class="el-menu-item-left"
                 v-for="(item,index) in menuList.children"
                 :key="index"
                 @click="handlerRoute(item,index)"
                 :class="{'active':lastRoute==item.code}"
             >
                 <span>{{item.name}}</span>
+                <div v-show="item.children">
+                    <p 
+                        v-for="(item,ind) in item.children"
+                        @click.stop="handlerRoute(item,index)"
+                        :key="ind"
+                        :class="{'active':lastRoute==item.code}"
+                        >
+                        {{item.name}}
+                    </p>
+                </div>
             </li>
         </ul>
     </el-aside>
 </template>
 <script>
-import { wechataccountDomain } from "@/environment/index";
+import { siteDomain } from "@/environment/index";
 export default {
     props: ["menuList", "lastRoute", "subTitle"],
     methods: {
         handlerRoute(item, index) {
-            this.$store.commit("SET_DIALOG",false)
+            //有三级路由时，二级路由不跳转
+            if (item.children && item.children.length > 0) {
+                return
+            }
             let domain = item.menuUrl.split("/")[0];
-            if (wechataccountDomain == domain) {
+            if (siteDomain == domain) {
                 this.$router.push(item.path);
+                return;
             } else {
                 window.location.href = "//" + item.menuUrl;
             }
-        }
+        },
     }
 };
 </script>
 <style scoped>
 .leftNavAside {
+    width: 100%;
     background: #f8fafc !important;
 }
 .submenu-title,
 .el-menu {
     background: #f8fafc;
 }
-.el-menu .el-menu-item {
-    height: 50px;
+.el-menu .el-menu-item-left {
     line-height: 50px;
     font-size: 14px;
-    margin-bottom: 10px;
+    text-align: left;
 }
-.el-menu :hover.el-menu-item {
+.el-menu :active.el-menu-item-left span{
+    color: #0595e6;
+}
+.el-menu .el-menu-item-left span{
+    display: inline-block;
+    height: 50px;
+    width: 100%;
+    line-height: 50px;
+    padding-left: 16px;
+    cursor: pointer;
+}
+.el-menu .el-menu-item-left p{
+    line-height: 50px;
+    padding-left: 40px;
+    cursor: pointer;
+}
+.el-menu .el-menu-item-left span:hover {
+    background: #fff;
+}
+.el-menu .el-menu-item-left p:hover {
     background: #fff;
 }
 .active {
@@ -59,9 +92,14 @@ export default {
     /* color: #0595e6; */
     background: #ffffff !important;
 }
+.active:active{
+     color: #0595e6;
+}
 .submenu-title {
     height: 40px;
     line-height: 40px;
     padding-top: 5px;
+    text-align: left;
+    padding-left: 16px;
 }
 </style>
