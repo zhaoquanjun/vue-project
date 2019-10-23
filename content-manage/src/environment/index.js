f196f5a117080954375da16a8a270c9908ea67a9
kevin <740172136@qq.com>
2019-10-23 17:08:46 +0800

    去掉域名的console,api/后面添加v1

--- content-manage/src/environment/index.js
+++ content-manage/src/environment/index.js
@@ -6,20 +6,20 @@ const baseServerUrl = window.location.hostname=='clouddream.net'?'clouddream.net
 const httpSchema = window.location.hostname=='clouddream.net'?'https://':'http://';
 
 /**生产环境-- */
-let contentApi = `${httpSchema}api.content.console.${baseServerUrl}`;
-let dashboardApi = `${httpSchema}api.dashboard.console.${baseServerUrl}`;
-let uploadPicUrl = `${httpSchema}api.content.console.${baseServerUrl}/api/picture`;
-let authorityUrl = `${httpSchema}login.console.${baseServerUrl}`;
-let redirectUrl = `${httpSchema}content.console.${baseServerUrl}/callback`;
-let logoutRedirectUri = `${httpSchema}content.console.${baseServerUrl}/index.html`;
-let silentRedirectUri = `${httpSchema}content.console.${baseServerUrl}/static/silent-renew.html`;
-let designerManageApi = `${httpSchema}api.designer.console.${baseServerUrl}`;
+let contentApi = `${httpSchema}api.content.${baseServerUrl}`;
+let dashboardApi = `${httpSchema}api.dashboard.${baseServerUrl}`;
+let uploadPicUrl = `${httpSchema}api.content.${baseServerUrl}/api/v1/picture`;
+let authorityUrl = `${httpSchema}login.${baseServerUrl}`;
+let redirectUrl = `${httpSchema}content.${baseServerUrl}/callback`;
+let logoutRedirectUri = `${httpSchema}content.${baseServerUrl}/index.html`;
+let silentRedirectUri = `${httpSchema}content.${baseServerUrl}/static/silent-renew.html`;
+let designerManageApi = `${httpSchema}api.designer.${baseServerUrl}`;
 let clientId = 'content.api';
 let scope = 'openid profile dashboard.api content.api designer.api produce.api weixinopen.api';
-export let siteDomain = `content.console.${baseServerUrl}`;
+export let siteDomain = `content.${baseServerUrl}`;
 // 个人中心
-export let personalUrl = `${httpSchema}dashboard.console.${baseServerUrl}/personal`;
-export let dashboardUrl = `${httpSchema}dashboard.console.${baseServerUrl}/board`;
+export let personalUrl = `${httpSchema}dashboard.${baseServerUrl}/personal`;
+export let dashboardUrl = `${httpSchema}dashboard.${baseServerUrl}/board`;
 // 阿里云续费地址
 export let aliMarketUrl = 'https://market.console.aliyun.com/imageconsole/index.htm?#/?_k=dd45g0';
 /**
@@ -27,14 +27,14 @@ export let aliMarketUrl = 'https://market.console.aliyun.com/imageconsole/index.
  */
 
 if (process.env.NODE_ENV === 'development') {
-    contentApi = `${httpSchema}api.content.console.wezhan.cn`;
-    dashboardApi = `${httpSchema}api.dashboard.console.wezhan.cn`;
-    uploadPicUrl = `${httpSchema}api.content.console.wezhan.cn/api/picture`;
-    authorityUrl = `${httpSchema}login.console.wezhan.cn`;
+    contentApi = `${httpSchema}api.content.wezhan.cn`;
+    dashboardApi = `${httpSchema}api.dashboard.wezhan.cn`;
+    uploadPicUrl = `${httpSchema}api.content.wezhan.cn/api/v1/picture`;
+    authorityUrl = `${httpSchema}login.wezhan.cn`;
     redirectUrl = `${httpSchema}localhost:8081/callback`;
-    logoutRedirectUri = `${httpSchema}content.console.wezhan.cn/index.html`;
+    logoutRedirectUri = `${httpSchema}content.wezhan.cn/index.html`;
     silentRedirectUri = `${httpSchema}localhost:8081/static/silent-renew.html`;
-    designerManageApi = `${httpSchema}api.designer.console.wezhan.cn`;
+    designerManageApi = `${httpSchema}api.designer.wezhan.cn`;
     clientId = 'dev.content.api';
     scope = 'openid profile dashboard.api content.api designer.api produce.api';
 } 
--- userapp/src/environment/index.js
+++ userapp/src/environment/index.js
@@ -2,49 +2,49 @@
 const baseServerUrl = window.location.hostname=='clouddream.net'?'clouddream.net':'wezhan.cn';
 const httpSchema = window.location.hostname=='clouddream.net'?'https://':'http://';
 
-let siteManageApi =`${httpSchema}api.designer.console.${baseServerUrl}`;
-let memberManageApi = `${httpSchema}api.dashboard.console.${baseServerUrl}`;
-let uploadPicUrl = `${httpSchema}api.content.console.${baseServerUrl}/api/picture`;
-let uploadRobotsUrl = `${httpSchema}api.designer.console.${baseServerUrl}/api/v1/Robots/AddOrOverride/`;
-let uploadSitemapUrl = `${httpSchema}api.designer.console.${baseServerUrl}/api/v1/SiteMapFile/AddOrOverride/`;
+let siteManageApi =`${httpSchema}api.designer.${baseServerUrl}`;
+let memberManageApi = `${httpSchema}api.dashboard.${baseServerUrl}`;
+let uploadPicUrl = `${httpSchema}api.content.${baseServerUrl}/api/v1/picture`;
+let uploadRobotsUrl = `${httpSchema}api.designer.${baseServerUrl}/api/v1/Robots/AddOrOverride/`;
+let uploadSitemapUrl = `${httpSchema}api.designer.${baseServerUrl}/api/v1/SiteMapFile/AddOrOverride/`;
 
 
-let authorityUrl = `${httpSchema}login.console.${baseServerUrl}`;
-let redirectUrl = `${httpSchema}dashboard.console.${baseServerUrl}/callback`
-let logoutRedirectUri = `${httpSchema}dashboard.console.${baseServerUrl}/index.html`;
-let silentRedirectUri = `${httpSchema}dashboard.console.${baseServerUrl}/static/silent-renew.html`;
+let authorityUrl = `${httpSchema}login.${baseServerUrl}`;
+let redirectUrl = `${httpSchema}dashboard.${baseServerUrl}/callback`
+let logoutRedirectUri = `${httpSchema}dashboard.${baseServerUrl}/index.html`;
+let silentRedirectUri = `${httpSchema}dashboard.${baseServerUrl}/static/silent-renew.html`;
 let clientId = `dashboard.api`;
 let scope = `openid profile dashboard.api content.api designer.api produce.api weixinopen.api`;
 
 
-export let designerManageApi = `${httpSchema}api.designer.console.${baseServerUrl}`;
-export let siteDomain = `dashboard.console.${baseServerUrl}`
+export let designerManageApi = `${httpSchema}api.designer.${baseServerUrl}`;
+export let siteDomain = `dashboard.${baseServerUrl}`
 export let shiftToAliyun = `https://help.aliyun.com/document_detail/54077.html?spm=a2c4g.11186623.2.10.61ca4c07hLv73n`;
 export let whyOpenCdn = `https://help.aliyun.com/document_detail/27101.html`;
-export let articleManageUrl = `${httpSchema}content.console.${baseServerUrl}/content/news`;
-export let productManageUrl = `${httpSchema}content.console.${baseServerUrl}/content/product`;
-export let imgManageUrl = `${httpSchema}content.console.${baseServerUrl}/content/picture`;
-export let fileManageUrl = `${httpSchema}content.console.${baseServerUrl}/content/file`;
-export let designerUrl = `${httpSchema}designer.console.${baseServerUrl}`;
-export let mySiteUrl = `${httpSchema}dashboard.console.${baseServerUrl}/website/mysite`;
-export let dashboardUrl = `${httpSchema}dashboard.console.${baseServerUrl}/board`;
+export let articleManageUrl = `${httpSchema}content.${baseServerUrl}/content/news`;
+export let productManageUrl = `${httpSchema}content.${baseServerUrl}/content/product`;
+export let imgManageUrl = `${httpSchema}content.${baseServerUrl}/content/picture`;
+export let fileManageUrl = `${httpSchema}content.${baseServerUrl}/content/file`;
+export let designerUrl = `${httpSchema}designer.${baseServerUrl}`;
+export let mySiteUrl = `${httpSchema}dashboard.${baseServerUrl}/website/mysite`;
+export let dashboardUrl = `${httpSchema}dashboard.${baseServerUrl}/board`;
 
 // 阿里云续费地址
 export let aliMarketUrl = "https://market.console.aliyun.com/imageconsole/index.htm?#/?_k=dd45g0";
 // 预览网站地图文件地址
-export let previewSitemapUrl = "http://api.designer.console.clouddream.net/api/v1/SiteMap/Preview/";
+export let previewSitemapUrl = `${httpSchema}api.designer.${baseServerUrl}/api/v1/SiteMap/Preview/`;
 //  图片地址
 export let picUrlHangZhou = 'https://wezhan.oss-cn-hangzhou.aliyuncs.com/'
 
 if (process.env.NODE_ENV === `development`) {
-    siteManageApi = `${httpSchema}api.designer.console.wezhan.cn`;
-    memberManageApi = `${httpSchema}api.dashboard.console.wezhan.cn`;
-    designerManageApi = `${httpSchema}api.designer.console.wezhan.cn`;
-    uploadPicUrl = `${httpSchema}api.content.console.wezhan.cn/api/picture`;
-    uploadRobotsUrl = `${httpSchema}api.designer.console.wezhan.cn/api/v1/Robots/AddOrOverride/`;
-    authorityUrl = `${httpSchema}login.console.wezhan.cn`;
+    siteManageApi = `${httpSchema}api.designer.wezhan.cn`;
+    memberManageApi = `${httpSchema}api.dashboard.wezhan.cn`;
+    designerManageApi = `${httpSchema}api.designer.wezhan.cn`;
+    uploadPicUrl = `${httpSchema}api.content.wezhan.cn/api/v1/picture`;
+    uploadRobotsUrl = `${httpSchema}api.designer.wezhan.cn/api/v1/Robots/AddOrOverride/`;
+    authorityUrl = `${httpSchema}login.wezhan.cn`;
     redirectUrl = `${httpSchema}localhost:8082/callback`;
-    logoutRedirectUri = `${httpSchema}dashboard.console.wezhan.cn/index.html`;
+    logoutRedirectUri = `${httpSchema}dashboard.wezhan.cn/index.html`;
     silentRedirectUri = `${httpSchema}localhost:8082/static/silent-renew.html`;
     clientId = "dev.dashboard.api";
     scope = "openid profile dashboard.api content.api designer.api produce.api";
