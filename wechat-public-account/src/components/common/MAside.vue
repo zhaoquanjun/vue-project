d9fa2415a682347997f1196c0b8ec3bedf3fb4b7
kevin <740172136@qq.com>
2019-10-23 15:26:04 +0800

    侧边栏优化

--- wechat-public-account/src/components/common/MAside.vue
+++ wechat-public-account/src/components/common/MAside.vue
@@ -13,7 +13,7 @@
                     :class="{'menu-bg':i==2,'menu-hover':curIndex==i}"
                     v-for="(it, i) in getMenuList"
                     :key="i"
-                    @mouseenter="changeCurHoverItem(i)"
+                    @mouseenter="changeCurHoverItem(it,i)"
                     @click="skipPages(it,i)"
                 >
                     <!--  :class="[curPath==it.code? it.code+'-on' : it.code,curIndex==i ? it.code+'-on' : it.code]" -->
@@ -64,8 +64,13 @@ export default {
         LeftNavComponents
     },
     methods: {
-        changeCurHoverItem(i) {
+        changeCurHoverItem(it,i) {
             this.curIndex = i;
+            if(it.children && it.children.length > 0) {
+                this.width = 300;
+            } else {
+                this.width = 150;
+            }
         },
         skipPages(item, i) {
             if (item.children && item.children.length> 0) {
@@ -76,18 +81,9 @@ export default {
         collapseOpen(width, time) {
             this.$store.commit("SET_DIALOG",true)
             this.width = 300;
-            this.width1 = 150;
-            this.display = "block";
-            this.time = time + "s";
-            this.border = "none";
         },
         collapseClose() {
             this.width = 60;
-            this.width1 = 0;
-            this.display = "none";
-            this.time = "0s";
-            this.curIndex = -1;
-            this.border = "1px solid #e5e5e5";
         },
         iconfonts(code) {
             switch (code) {
