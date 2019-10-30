dfac9f5bb10708241d51ef5568380664706acb8d
duhan <18910332606@163.com>
2019-10-30 20:48:38 +0800

    微信siteid

--- wechat-public-account/src/views/wechat-account/accountwxauther.vue
+++ wechat-public-account/src/views/wechat-account/accountwxauther.vue
@@ -128,7 +128,7 @@ export default {
     },
     // 获取当前可选域名列表
     async _getCdnDomainList() {
-      await this.$store.dispatch('_setSiteId')
+      // await this.$store.dispatch('_setSiteId')
       await this.$store.dispatch('_getWxStatus')
       let wx_status = this.$store.getters.wx_status;
       if (wx_status.isAuth && wx_status.isCertification && wx_status.isResolveSuccess) {
