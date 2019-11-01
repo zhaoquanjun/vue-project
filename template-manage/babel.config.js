fe80395e1dbcde3ea68d6244148871cfc745c35c
kevin <740172136@qq.com>
2019-11-01 14:40:25 +0800

    babel.config修改

--- template-manage/babel.config.js
+++ template-manage/babel.config.js
@@ -1,7 +1,13 @@
 module.exports = {
     "presets": [
       "@vue/app",
-      ['@babel/preset-env',{'modules':false}]
+      {
+        "polyfills": [
+          'es6.promise',
+          'es6.symbol'
+        ]
+      }
+      //['@babel/preset-env',{'modules':false}]
     ],
   "plugins": [
     [
