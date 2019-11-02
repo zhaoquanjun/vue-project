94ed0408422fdf60bbe277e5a7a5f50d971dfe50
kevin <740172136@qq.com>
2019-11-02 14:27:33 +0800

    sdn修改

--- wechat-public-account/vue.config.js
+++ wechat-public-account/vue.config.js
@@ -9,21 +9,23 @@ module.exports = {
         config.resolve.alias.set('_c', path.resolve(__dirname, 'src/components'));
         config.resolve.alias.set('_v', path.resolve(__dirname, 'src/views'));
         config.resolve.alias.set('img', path.resolve(__dirname, 'static/images'));
+        
     },
+    // configureWebpack: config => {
+    //     config.externals = {
+    //         'vue':'Vue',
+    //         'vuex':'Vuex',
+    //         'vue-router':'VueRouter',
+    //         'axios':'axios',
+    //         'element-ui': 'ELEMENT',
+    //     }
+    // },
     
     devServer: {
         disableHostCheck: true,
         port: 8084,
-    },
-    configureWebpack: config => {
-        config.externals = {
-            'vue':'Vue',
-            'vuex':'Vuex',
-            'vue-router':'VueRouter',
-            'axios':'axios',
-            'element-ui': 'ELEMENT',
-        }
     }
+    
 }
 
 
