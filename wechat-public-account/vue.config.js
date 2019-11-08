b107cf2e60273146bd845625c0469174c596c535
WangYiGuang <740172136@qq.com>
2019-11-08 17:16:18 +0800

    cdn修改

--- wechat-public-account/vue.config.js
+++ wechat-public-account/vue.config.js
@@ -1,4 +1,15 @@
 let path = require('path');
+const isProduction = process.env.NODE_ENV === 'production';
+const cdn = {
+    css: [],
+    js: [
+        "https://cdn.bootcss.com/vue/2.6.10/vue.min.js",
+        "https://cdn.bootcss.com/vue-router/3.0.7/vue-router.min.js",
+        "https://cdn.bootcss.com/axios/0.19.0/axios.min.js",
+        "https://cdn.bootcss.com/element-ui/2.11.1/index.js",
+        "https://cdn.bootcss.com/vuex/3.1.1/vuex.min.js"
+    ]
+}
 module.exports = {
     publicPath: '/',
     // 打包 不再使用sourcemap
@@ -9,21 +20,37 @@ module.exports = {
         config.resolve.alias.set('_c', path.resolve(__dirname, 'src/components'));
         config.resolve.alias.set('_v', path.resolve(__dirname, 'src/views'));
         config.resolve.alias.set('img', path.resolve(__dirname, 'static/images'));
-        
+        // 生产环境配置
+        if (isProduction) {
+            // 压缩代码
+            config.optimization.minimize(true);
+            // 分割代码
+            config.optimization.splitChunks({
+                chunks: 'all'
+            })
+            // 生产环境注入cdn
+            config.plugin('html')
+                .tap(args => {
+                    args[0].cdn = cdn;
+                    return args;
+                });
+        }
     },
-    // configureWebpack: config => {
-    //     config.externals = {
-    //         'vue':'Vue',
-    //         'vuex':'Vuex',
-    //         'vue-router':'VueRouter',
-    //         'axios':'axios',
-    //         'element-ui': 'ELEMENT',
-    //     }
-    // },
-    
     devServer: {
         disableHostCheck: true,
         port: 8084,
+    },
+    configureWebpack: config => {
+        // 生产环境配置
+        if (isProduction) {
+            config.externals = {
+                'vue':'Vue',
+                'vuex':'Vuex',
+                'vue-router':'VueRouter',
+                'axios':'axios',
+                'element-ui': 'ELEMENT',
+            }
+        };
     }
     
 }
